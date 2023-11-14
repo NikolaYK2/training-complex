1fc77a8f47784f1eede9b0971bd7a8af2d61d76f
k2 <izmail.k2@gmail.com>
2023-11-14 22:46:09 +0300

    add DropDownMenu Component map

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
-import {DropDownMenu} from "@/commn/components/ui/dropDownMenu/DropDownMenu.tsx";
+import {ArrType, DropDownMenu} from "@/commn/components/ui/dropDownMenu/DropDownMenu.tsx";
 
 export function App() {
-const prof= {id:1, name:'icon',nameProfile:'Nikolaj', email:'Kev@', profile:'My Profile', logOut:'Sing Out'}
-const card= {
-  id:1,
-  name:'icon',
-  learn: 'learn',
-  edit: 'Edit',
-  delete: 'delete'
-}
+  const prof = {
+    id: 1,
+    name: 'icon',
+    arr: [
+      {icon: 'avatar', name: 'Nikolaj', email: 'Kev'},
+      {icon: 'profile', name: 'My Profile'},
+      {icon: 'logOut', name: 'Sing Out'},
+    ] as ArrType[]
+  }
+  const card = {
+    id: 1,
+    name: 'icon',
+    arr: [
+      {icon: 'learn', name: 'Learn'},
+      {icon: 'edit', name: 'Edit'},
+      {icon: 'delete', name: 'Delete'},
+    ] as ArrType[]
+  }
 
 
-  return <div style={{display:"flex", justifyContent: "center", alignItems:"center", height:'400px'}}>
+  return <div style={{display: "flex", justifyContent: "center", alignItems: "center", height: '400px'}}>
     <DropDownMenu obj={prof}/>
   </div>
 }
--- src/commn/components/ui/dropDownMenu/DropDownMenu.tsx
+++ src/commn/components/ui/dropDownMenu/DropDownMenu.tsx
@@ -1,20 +1,18 @@
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {FC} from "react";
 import {IconSvg} from "@/commn/components/ui/iconSvg/IconSvg.tsx";
-import ava from '@/assets/image/logo/ava.png'
 import s from './DropDownMenu.module.scss'
 
 
 type MenuType = {
   id: number,
   name: string,
-  nameProfile?: string,
-  email?: string,
-  profile?: string,
-  logOut?: string,
-  learn?: string,
-  edit?: string,
-  delete?: string
+  arr: ArrType[],
+}
+export type ArrType = {
+  icon: 'learn' | 'edit' | 'delete' | 'profile' | 'logOut' | 'avatar',
+  name: string,
+  email?: string
 }
 type Props = {
   obj: MenuType,
@@ -32,31 +30,60 @@ export const DropDownMenu: FC<Props> = ({obj}) => {
 
       <DropdownMenu.Portal>
         <DropdownMenu.Content className={s.content} sideOffset={5}>
-          <DropdownMenu.Item className={s.item}>
-            {obj.email ?
-              <div className={s.avatar}>
-                <img src={ava} alt=""/>
-              </div> :
-              <div className={s.iconSlot}>
-                <IconSvg name={'learn'}/>
-              </div>}
-            <div className={s.text}>
-              {(obj.nameProfile && obj.email) ? <><span>{obj.nameProfile}</span><span>{obj.email}</span> </> : obj.learn}
-            </div>
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className={s.item}>
-            <div className={s.iconSlot}>
-              <IconSvg name={obj.profile ? 'profile' : 'edit'}/>
-            </div>
-            {obj.profile || obj.edit}
-          </DropdownMenu.Item>
-          <DropdownMenu.Item className={s.item}>
-            <div className={s.iconSlot}><IconSvg name={obj.logOut ? 'logOut' : 'delete'}/></div>
-            {obj.logOut || obj.delete}
-          </DropdownMenu.Item>
+          {obj.arr.map(el =>
+            <DropdownMenu.Item className={s.item} key={el.icon}>
+                <div className={el.email ? s.avatar : s.iconSlot}>
+                  <IconSvg name={el.icon}/>
+                </div>
+              <div className={s.text}>
+                <span>{el.name}</span>
+                <span>{el.email}</span>
+              </div>
+            </DropdownMenu.Item>
+          )}
           <DropdownMenu.Arrow className={s.menuArrow}/>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
   );
 };
+// export const DropDownMenu: FC<Props> = ({obj}) => {
+//
+//   return (
+//     <DropdownMenu.Root>
+//       <DropdownMenu.Trigger asChild>
+//         <button className="IconButton" aria-label="Customise options">
+//           {obj.name}
+//         </button>
+//       </DropdownMenu.Trigger>
+//
+//       <DropdownMenu.Portal>
+//         <DropdownMenu.Content className={s.content} sideOffset={5}>
+//           <DropdownMenu.Item className={s.item}>
+//             {obj.email ?
+//               <div className={s.avatar}>
+//                 <img src={ava} alt=""/>
+//               </div> :
+//               <div className={s.iconSlot}>
+//                 <IconSvg name={'learn'}/>
+//               </div>}
+//             <div className={s.text}>
+//               {(obj.nameProfile && obj.email) ? <><span>{obj.nameProfile}</span><span>{obj.email}</span> </> : obj.learn}
+//             </div>
+//           </DropdownMenu.Item>
+//           <DropdownMenu.Item className={s.item}>
+//             <div className={s.iconSlot}>
+//               <IconSvg name={obj.profile ? 'profile' : 'edit'}/>
+//             </div>
+//             {obj.profile || obj.edit}
+//           </DropdownMenu.Item>
+//           <DropdownMenu.Item className={s.item}>
+//             <div className={s.iconSlot}><IconSvg name={obj.logOut ? 'logOut' : 'delete'}/></div>
+//             {obj.logOut || obj.delete}
+//           </DropdownMenu.Item>
+//           <DropdownMenu.Arrow className={s.menuArrow}/>
+//         </DropdownMenu.Content>
+//       </DropdownMenu.Portal>
+//     </DropdownMenu.Root>
+//   );
+// };
