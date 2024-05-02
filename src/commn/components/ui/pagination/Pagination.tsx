5a982ad889709352fd01fc631e63c67491257e86
k2 <izmail.k2@gmail.com>
2024-05-02 22:06:21 +0300

    refactor: select props options value string

--- src/commn/components/ui/pagination/Pagination.tsx
+++ src/commn/components/ui/pagination/Pagination.tsx
@@ -67,11 +67,11 @@ export const Pagination: FC<Props> = ({
 
       <div className={s.blockSelector}>
         <Select options={[
-          {id: 10, value: 10,},
-          {id: 20, value: 20,},
-          {id: 30, value: 30,},
-          {id: 50, value: 50,},
-          {id: 100, value: 100,},
+          {id: 10, value: '10',},
+          {id: 20, value: '20',},
+          {id: 30, value: '30',},
+          {id: 50, value: '50',},
+          {id: 100, value: '100',},
         ]} setOptions={setPageSize}/>
       </div>
     </nav>
