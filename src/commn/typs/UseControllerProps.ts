ae0be21f6c3da786c8a1f0580b91c11d34577aca
k2 <izmail.k2@gmail.com>
2024-05-07 16:43:40 +0300

    refactor: Eslint

--- src/commn/typs/UseControllerProps.ts
+++ src/commn/typs/UseControllerProps.ts
@@ -1,11 +1,16 @@
-import {Control, FieldPath, FieldPathValue, FieldValues, RegisterOptions} from "react-hook-form";
+import { Control, FieldPath, FieldPathValue, FieldValues, RegisterOptions } from 'react-hook-form'
 
-export type UseControllerProps<TFieldValues extends FieldValues = FieldValues, TName extends FieldPath<TFieldValues>
-  = FieldPath<TFieldValues>> = {
-  name: TName;
-  rules?: Omit<RegisterOptions<TFieldValues, TName>, 'valueAsNumber' | 'valueAsDate' | 'setValueAs' | 'disabled'>;
-  shouldUnregister?: boolean;
-  defaultValue?: FieldPathValue<TFieldValues, TName>;
-  control: Control<TFieldValues>;
-  disabled?: boolean;
-};
+export type UseControllerProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> = {
+  control: Control<TFieldValues>
+  defaultValue?: FieldPathValue<TFieldValues, TName>
+  disabled?: boolean
+  name: TName
+  rules?: Omit<
+    RegisterOptions<TFieldValues, TName>,
+    'disabled' | 'setValueAs' | 'valueAsDate' | 'valueAsNumber'
+  >
+  shouldUnregister?: boolean
+}
