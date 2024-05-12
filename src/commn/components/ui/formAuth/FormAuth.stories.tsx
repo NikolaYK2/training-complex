f816dc5cdfb771a09fd2825640c09400c06f2f29
k2 <izmail.k2@gmail.com>
2024-05-12 14:28:11 +0300

    feat: add form checkemail for watching useful message mail

--- src/commn/components/ui/formAuth/FormAuth.stories.tsx
+++ src/commn/components/ui/formAuth/FormAuth.stories.tsx
@@ -2,6 +2,14 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
+import {
+  CreateNewPasswordSchema,
+  CreateNewPasswordType,
+} from '@/features/auth/createPassword/CreateNewPassword'
+import {
+  ForgotPasswordType,
+  forgotPasswordSchema,
+} from '@/features/auth/forgotPassword/ForgotPassword'
 import { LoginFormType, loginSchema } from '@/features/auth/login/SignIn'
 import { RegisterFormType, RegisterSchema } from '@/features/auth/registration/SignUp'
 import { zodResolver } from '@hookform/resolvers/zod'
@@ -52,7 +60,7 @@ const FormLogin = () => {
 export const Login: Story = {
   render: () => <FormLogin />,
 }
-
+//-----------------------------------------------------------------
 const FormRegister = () => {
   const {
     control,
@@ -88,20 +96,20 @@ const FormRegister = () => {
 export const Register: Story = {
   render: () => <FormRegister />,
 }
-
+//--------------------------------------------------------------------------
 const FormForgotPassword = () => {
   const {
     control,
     formState: { errors },
     handleSubmit,
     reset,
-  } = useForm<any>({
+  } = useForm<ForgotPasswordType>({
     defaultValues: {
       email: '',
     },
-    // resolver: zodResolver(RegisterSchema),
+    resolver: zodResolver(forgotPasswordSchema),
   })
-  const onSubmit: SubmitHandler<any> = data => {
+  const onSubmit: SubmitHandler<ForgotPasswordType> = data => {
     console.log(data)
     reset()
   }
@@ -122,20 +130,20 @@ const FormForgotPassword = () => {
 export const ForgotPassword: Story = {
   render: () => <FormForgotPassword />,
 }
-
+//-----------------------------------------------------------------
 const FormCreateNewPassword = () => {
   const {
     control,
     formState: { errors },
     handleSubmit,
     reset,
-  } = useForm<any>({
+  } = useForm<CreateNewPasswordType>({
     defaultValues: {
-      email: '',
+      password: '',
     },
-    // resolver: zodResolver(RegisterSchema),
+    resolver: zodResolver(CreateNewPasswordSchema),
   })
-  const onSubmit: SubmitHandler<any> = data => {
+  const onSubmit: SubmitHandler<CreateNewPasswordType> = data => {
     console.log(data)
     reset()
   }
@@ -155,4 +163,16 @@ const FormCreateNewPassword = () => {
 
 export const CreateNewPassword: Story = {
   render: () => <FormCreateNewPassword />,
+}
+//-----------------------------------------------------------------
+const FormCheckEmail = () => {
+  return (
+    <Card>
+      <FormAuth title={'check email'} />
+    </Card>
+  )
+}
+
+export const CheckEmail: Story = {
+  render: () => <FormCheckEmail />,
 }
--- src/commn/components/ui/formAuth/FormAuth.tsx
+++ src/commn/components/ui/formAuth/FormAuth.tsx
@@ -3,13 +3,27 @@ import { Control, FieldErrors, FieldValues, Path } from 'react-hook-form'
 
 import { Button } from '@/commn/components/ui/button'
 import { ControlledCheckbox } from '@/commn/components/ui/checkBox/ControlledCheckbox'
+import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
 import { ControlledTextField } from '@/commn/components/ui/input/ControlledTextField'
 import { AuthRedirectLink } from '@/commn/components/ui/redirectAuth/AuthRedirectLink'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 
 import s from './FormAuth.module.scss'
 
-const formInfo = {
+type ItemType = {
+  bottomName?: string
+  description: string
+  redirect: boolean
+}
+type FormInfoType = {
+  [key: string]: ItemType
+}
+const formInfo: FormInfoType = {
+  'check email': {
+    bottomName: 'Back to Sign In',
+    description: 'We’ve sent an Email with instructions to example@mail.com',
+    redirect: false,
+  },
   'create new password': {
     description: 'Create new password and we will send you further instructions to email',
     redirect: false,
@@ -22,14 +36,19 @@ const formInfo = {
   'sign up': { description: '', redirect: true },
 }
 
-export type TitleType = 'create new password' | 'forgot your password?' | 'sign in' | 'sign up'
+export type TitleType =
+  | 'check email'
+  | 'create new password'
+  | 'forgot your password?'
+  | 'sign in'
+  | 'sign up'
 
 type Props<TFieldValues extends FieldValues> = {
   className?: string
-  control: Control<TFieldValues>
-  errorMessage: FieldErrors<TFieldValues>
-  formItem: (Path<TFieldValues> & string)[]
-  onSubmit: FormEventHandler<HTMLFormElement>
+  control?: Control<TFieldValues>
+  errorMessage?: FieldErrors<TFieldValues>
+  formItem?: (Path<TFieldValues> & string)[]
+  onSubmit?: FormEventHandler<HTMLFormElement>
   title: TitleType
 }
 
@@ -41,37 +60,52 @@ export const FormAuth = <TFieldValues extends FieldValues>({
   onSubmit,
   title,
 }: Props<TFieldValues>) => {
+  const checkEmail = title === 'check email'
+
   return (
     <form className={`${s.form} ${className}`} noValidate onSubmit={onSubmit}>
-      <TextFormat className={s.title} variant={'h1'}>
+      <TextFormat className={`${s.title} ${title.length > 11 && s.titleMod}`} variant={'h1'}>
         {title}
       </TextFormat>
-      {formItem.map(input =>
-        input === 'remember' ? (
-          <ControlledCheckbox control={control} key={input} label={'remember me'} name={input} />
-        ) : (
-          <ControlledTextField
-            control={control}
-            errorMessage={errorMessage[input]?.message as string}
-            key={input}
-            label={input}
-            name={input}
-            type={input === 'passwordConfirm' ? 'password' : input}
-          />
-        )
-      )}
+      {formItem &&
+        control &&
+        errorMessage &&
+        formItem.map(input =>
+          input === 'remember' ? (
+            <ControlledCheckbox control={control} key={input} label={'remember me'} name={input} />
+          ) : (
+            <ControlledTextField
+              control={control}
+              errorMessage={errorMessage[input]?.message as string}
+              key={input}
+              label={input}
+              name={input}
+              type={input === 'passwordConfirm' ? 'password' : input}
+            />
+          )
+        )}
 
       {title === 'sign in' && (
         <AuthRedirectLink className={s.link} title={'forgot your password?'} />
       )}
 
-      {formInfo[title].description}
+      {checkEmail && (
+        <div className={s.checkEmail}>
+          <IconSvg name={'checkEmail'} />
+        </div>
+      )}
+
+      {formInfo[title] && (
+        <TextFormat className={`${s.description} ${checkEmail && s.positionMod}`} variant={'body2'}>
+          {formInfo[title].description}
+        </TextFormat>
+      )}
 
       <Button className={s.submitBtn} type={'submit'} variant={'primary'}>
-        {title}
+        {checkEmail ? 'Back to Sign In' : title}
       </Button>
 
-      {formInfo[title].redirect && <AuthRedirectLink className={s.description} title={title} />}
+      {formInfo[title].redirect && <AuthRedirectLink className={s.redirect} title={title} />}
     </form>
   )
 }
