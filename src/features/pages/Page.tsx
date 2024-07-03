5ae241f92e7014f58cd9f14139bfd7f0703fd364
k2 <izmail.k2@gmail.com>
2024-07-03 20:51:51 +0300

    feat: add Page.tsx

---
+++ src/features/pages/Page.tsx
@@ -0,0 +1,15 @@
+import { CSSProperties, ComponentPropsWithoutRef } from 'react'
+
+import clsx from 'clsx'
+
+import s from './Page.module.scss'
+
+type Props = ComponentPropsWithoutRef<'div'> & {
+  marginTop?: CSSProperties['marginTop']
+}
+export const Page = ({ className, marginTop = '33px', style, ...rest }: Props) => {
+  const classes = clsx(className, s.pageContainer)
+  const styles: CSSProperties = { marginTop: marginTop, ...style }
+
+  return <div className={classes} style={styles} {...rest} /> //тут чилдрен уже будет в rest
+}
