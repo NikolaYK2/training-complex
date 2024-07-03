0241ffab3647e6944fb36d9e509639976a507ff9
k2 <izmail.k2@gmail.com>
2024-07-03 20:49:26 +0300

    refactor: structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,8 @@
-import {App} from '@/App'
-import {createRoot} from 'react-dom/client'
+import { App } from '@/App'
+import { createRoot } from 'react-dom/client'
 
 import '@fontsource/roboto/400.css'
 import '@fontsource/roboto/700.css'
 import '@/assets/styles/index.scss'
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-    <App />
-)
+createRoot(document.getElementById('root') as HTMLElement).render(<App />)
