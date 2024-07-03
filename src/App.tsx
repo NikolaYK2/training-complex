58006c3d84d3d4fcf76cd05b8365f2c3f5de9997
k2 <izmail.k2@gmail.com>
2024-07-03 20:44:25 +0300

    feat: Add Layout Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,10 @@
-import { Select } from '@/commn/components/ui/select/Select'
+import { Layout } from '@/commn/components/ui/layout/Layout'
+import { Decks } from '@/features/decks/Decks'
 
 export function App() {
   return (
-    <div
-      style={{ alignItems: 'center', display: 'flex', height: '400px', justifyContent: 'center' }}
-    >
-      <Select
-        options={[
-          { id: 1, value: 'Select-box' },
-          { id: 2, value: 'hi maloy' },
-          { id: 3, value: 'Sam maloy' },
-        ]}
-      />
-    </div>
+    <Layout>
+      <Decks />
+    </Layout>
   )
 }
