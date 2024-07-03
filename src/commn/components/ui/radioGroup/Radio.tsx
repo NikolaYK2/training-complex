4d90dd9b720be256a692f2c9fc971079db438580
k2 <izmail.k2@gmail.com>
2024-07-03 20:52:20 +0300

    chore: refactor

--- src/commn/components/ui/radioGroup/Radio.tsx
+++ src/commn/components/ui/radioGroup/Radio.tsx
@@ -1,22 +1,24 @@
-import * as RadioGroup from '@radix-ui/react-radio-group';
-import {FC} from "react";
+import { FC } from 'react'
+
+import * as RadioGroup from '@radix-ui/react-radio-group'
+
 import s from './Radio.module.scss'
 
 type Props = {
+  disabled?: boolean
   id: string
   value: string
-  disabled?: boolean
 }
 
-export const Radio: FC<Props> = ({disabled, value, id}) => {
+export const Radio: FC<Props> = ({ disabled, id, value }) => {
   return (
     <div className={`${s.blockRadio}`}>
       <label className={`${s.blockItem} ${disabled ? s.disabledLabel : ''}`}>
-        <RadioGroup.Item className={s.item} value={value} id={id}>
-          <RadioGroup.Indicator className={s.indicator}/>
+        <RadioGroup.Item className={s.item} id={id} value={value}>
+          <RadioGroup.Indicator className={s.indicator} />
         </RadioGroup.Item>
       </label>
       <label className={`${s.label} ${disabled ? s.disabledLabel : ''}`}>{value}</label>
     </div>
-  );
-};
+  )
+}
