6a70054c23a5e3277d338224fbf7acdc375808a9
k2 <izmail.k2@gmail.com>
2024-07-08 01:00:05 +0300

    feat: breakdown by entity endpoints

---
+++ src/services/decks/decksService.ts
@@ -0,0 +1,40 @@
+import { CreateDeckArgs, Deck, DecksResponse, GetDecksArgs } from '@/services/decks/DecksTypes'
+import { flashcardsApi } from '@/services/flashcardsApi'
+
+export const DecksService = flashcardsApi.injectEndpoints({
+  endpoints: builder => ({
+    createDeck: builder.mutation<Deck, CreateDeckArgs>({
+      invalidatesTags: ['Decks'],
+      query: args => ({
+        body: args,
+        method: 'POST', //по default GET
+        url: `v1/decks`,
+      }),
+    }),
+    getDeckById: builder.query<DecksResponse, { id: string }>({
+      //uri params
+      query: ({ id }) => {
+        return {
+          url: `v2/decks/${id}`,
+        }
+      },
+    }),
+    getDecks: builder.query<DecksResponse, GetDecksArgs | void>({
+      providesTags: ['Decks'], //как бы обновляем кэш, так как новые данные
+      //query param
+      query: params => {
+        return {
+          params: params ?? {},
+          url: `v2/decks`,
+        }
+      },
+    }),
+  }),
+})
+
+export const {
+  useCreateDeckMutation,
+  useGetDeckByIdQuery,
+  useGetDecksQuery,
+  useLazyGetDecksQuery,
+} = DecksService
