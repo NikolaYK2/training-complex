f14b3c0c45461cc662de1e5bf62cd59ea6565640
k2 <izmail.k2@gmail.com>
2024-07-07 22:24:46 +0300

    chore: refactor

--- src/commn/hooks/usePagination.ts
+++ src/commn/hooks/usePagination.ts
@@ -1,42 +1,43 @@
-import {useMemo} from "react";
+import { useMemo } from 'react'
 
 export const DOTS = '...'
 
 const range = (start: number, end: number) => {
-  let length = end - start + 1;
-  return Array.from({length}, (_, idx) => idx + start);
-};
+  const length = end - start + 1
+
+  return Array.from({ length }, (_, idx) => idx + start)
+}
 
 type PaginationArgs = {
-  totalCount: number; // Общее количество элементов
-  pageSize: number; // Количество элементов на странице
-  currentPage: number; // Текущая страница
-  siblingCount: number; // Количество "соседних" страниц, отображаемых слева и справа от текущей страницы
-};
+  currentPage: number // Текущая страница
+  pageSize: number // Количество элементов на странице
+  siblingCount: number // Количество "соседних" страниц, отображаемых слева и справа от текущей страницы
+  totalCount: number // Общее количество элементов
+}
 
 export const usePagination = ({
-                                totalCount,
-                                pageSize,
-                                siblingCount = 1,
-                                currentPage,
-                              }: PaginationArgs) => {
+  currentPage,
+  pageSize,
+  siblingCount = 1,
+  totalCount,
+}: PaginationArgs) => {
   // Вычисляем общее количество страниц
-  const totalPageCount = Math.ceil(totalCount / pageSize);
+  const totalPageCount = Math.ceil(totalCount / pageSize)
 
   // Общее количество номеров страниц, которые мы хотим показать
-  const totalPageNumbers = siblingCount + 5;
+  const totalPageNumbers = siblingCount + 5
 
   // Вычисляем индексы левого и правого "соседей" текущей страницы
-  const leftSiblingIndex = Math.max(currentPage - siblingCount, 1);
-  const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount);
+  const leftSiblingIndex = Math.max(currentPage - siblingCount, 1)
+  const rightSiblingIndex = Math.min(currentPage + siblingCount, totalPageCount)
 
   // Определяем, нужно ли показывать многоточие слева и справа от текущей страницы
-  const shouldShowLeftDots = leftSiblingIndex > 2;
-  const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2;
+  const shouldShowLeftDots = leftSiblingIndex > 2
+  const shouldShowRightDots = rightSiblingIndex < totalPageCount - 2
 
   // Первая и последняя страницы всегда отображаются
-  const firstPageIndex = 1;
-  const lastPageIndex = totalPageCount;
+  const firstPageIndex = 1
+  const lastPageIndex = totalPageCount
 
   // Используем useMemo для оптимизации вычисления диапазона страниц
   // Возвращаем диапазон страниц
@@ -44,35 +45,33 @@ export const usePagination = ({
     // Если общее количество страниц меньше или равно количеству страниц, которые мы хотим показать,
     // то просто возвращаем все страницы
     if (totalPageNumbers >= totalPageCount) {
-      return range(1, totalPageCount);
+      return range(1, totalPageCount)
     }
 
     // Если не нужно показывать многоточие слева, но нужно справа,
     // то возвращаем первые страницы и многоточие перед последней страницей
     if (!shouldShowLeftDots && shouldShowRightDots) {
-      let leftItemCount = 3 + 2 * siblingCount;
-      let leftRange = range(1, leftItemCount);
+      const leftItemCount = 3 + 2 * siblingCount
+      const leftRange = range(1, leftItemCount)
 
-      return [...leftRange, DOTS, totalPageCount];
+      return [...leftRange, DOTS, totalPageCount]
     }
 
     // Если нужно показывать многоточие слева, но не справа,
     // то возвращаем первую страницу, многоточие и последние страницы
     if (shouldShowLeftDots && !shouldShowRightDots) {
-      let rightItemCount = 3 + 2 * siblingCount;
-      let rightRange = range(
-        totalPageCount - rightItemCount + 1,
-        totalPageCount
-      );
-      return [firstPageIndex, DOTS, ...rightRange];
+      const rightItemCount = 3 + 2 * siblingCount
+      const rightRange = range(totalPageCount - rightItemCount + 1, totalPageCount)
+
+      return [firstPageIndex, DOTS, ...rightRange]
     }
 
     // Если нужно показывать многоточие и слева, и справа,
     // то возвращаем первую страницу, многоточие, соседние страницы, многоточие и последнюю страницу
     if (shouldShowLeftDots && shouldShowRightDots) {
-      let middleRange = range(leftSiblingIndex, rightSiblingIndex);
-      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex];
-    }
+      const middleRange = range(leftSiblingIndex, rightSiblingIndex)
 
-  }, [totalCount, pageSize, siblingCount, currentPage]);
-};
+      return [firstPageIndex, DOTS, ...middleRange, DOTS, lastPageIndex]
+    }
+  }, [totalCount, pageSize, siblingCount, currentPage])
+}
