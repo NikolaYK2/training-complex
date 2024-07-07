91e37b42183e3c799809ccf2e56b71ed15664a7c
k2 <izmail.k2@gmail.com>
2024-07-08 01:03:13 +0300

    refactor: Add pagination - Add TextField - searchParams application

--- src/features/decks/Decks.tsx
+++ src/features/decks/Decks.tsx
@@ -1,23 +1,96 @@
+import { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
+
 import { Button } from '@/commn/components/ui/button'
 import { TextField } from '@/commn/components/ui/input/TextField'
+import { Pagination } from '@/commn/components/ui/pagination/Pagination'
 import { SliderValue } from '@/commn/components/ui/slider/SliderValue'
 import { TabSwitcher } from '@/commn/components/ui/tabSwitcher/TabSwitcher'
 import { Table } from '@/commn/components/ui/tables/Table'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 import { Page } from '@/features/pages/Page'
+import { useCreateDeckMutation, useGetDecksQuery } from '@/services/decks/decksService'
 
 import s from './Decks.module.scss'
 
 export const Decks = () => {
+  //rrm search params ------------
+  const [searchParams, setSearchParams] = useSearchParams({ name: '', page: '1' }) //МОжем испю вместо стейта хз зачем
+
+  const page = Number(searchParams.get('page'))
+  const name = searchParams.get('name')
+  const setPage = (page: number) => {
+    searchParams.set('page', page.toString())
+    setSearchParams(searchParams)
+    // setSearchParams({ page: page.toString() })
+  }
+  const setSearch = (name: string) => {
+    if (name === '') {
+      searchParams.delete('name')
+    } else {
+      searchParams.set('name', name)
+    }
+    searchParams.set('page', '1')
+
+    setSearchParams(searchParams)
+  }
+
+  const [itemPage, setItemPage] = useState(10)
+
+  const { data, error, isError, isLoading } = useGetDecksQuery({
+    // как useEffect поулчаем данные
+    currentPage: page || 1, //было просто page если use useState
+    itemsPerPage: itemPage,
+    name: name ?? undefined,
+  }) //из query возвращается обьект из mutation картэш(массив с заранее определенными элементами)
+
+  //картешь, первым элементам у нас функция а вторым обьект с данными что приходят
+  const [createDeck, { isLoading: isLoadingCreatedDeck }] = useCreateDeckMutation()
+
+  //table ----------------------
+  const headers = [
+    { id: 1, title: 'Name' },
+    { id: 2, title: 'Cards' },
+    { id: 3, title: 'Last Updated' },
+    { id: 4, title: 'Created By' },
+  ]
+
+  const paragraphs =
+    data?.items.map(deck => ({
+      cells: [
+        { value: deck.name },
+        { value: `${deck.cardsCount}` },
+        { value: new Date(deck.updated).toLocaleDateString() },
+        { value: deck.author.name },
+      ],
+      idCells: deck.id,
+    })) || []
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+  if (isError) {
+    console.error(error)
+
+    return <div>Error</div>
+  }
+
   return (
     <Page>
       <div className={s.deck}>
         <TextFormat variant={'h1'}>Decks</TextFormat>
-        <Button>Add new deck</Button>
+        <Button disabled={isLoadingCreatedDeck} onClick={() => createDeck({ name: '123' })}>
+          {isLoadingCreatedDeck ? 'loading' : 'Add new deck'}
+        </Button>
       </div>
       <div className={s.filters}>
         <div className={s.search}>
-          <TextField type={'search'} />
+          <TextField
+            onValueChange={setSearch}
+            placeholder={'Search...'}
+            type={'search'}
+            value={name ?? ''}
+          />
         </div>
         <div className={s.tab}>
           <TabSwitcher
@@ -33,23 +106,16 @@ export const Decks = () => {
         <Button variant={'secondary'}>Clear</Button>
       </div>
       <div className={s.table}>
-        <Table
-          headers={[
-            { id: 1, title: 'name' },
-            { id: 2, title: 'cards' },
-            { id: 3, sort: true, title: 'last updated' },
-            { id: 4, title: 'Created by' },
-          ]}
-          paragraphs={[
-            [
-              { id: 1, title: 'Pack Name' },
-              { id: 2, title: '10' },
-              { id: 3, title: '18.03.2024' },
-              { id: 4, title: 'Kev K2' },
-            ],
-          ]}
-        />
+        <Table headers={headers} paragraphs={paragraphs} />
       </div>
+      <Pagination
+        currentPage={page}
+        onPageChange={setPage}
+        pageSize={1}
+        setPageSize={setItemPage}
+        siblingCount={1}
+        totalCount={data?.pagination?.totalPages ?? 1}
+      />
     </Page>
   )
 }
