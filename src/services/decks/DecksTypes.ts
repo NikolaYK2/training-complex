f4da428adad8146d841f2e6b0855fce795bcf2f4
k2 <izmail.k2@gmail.com>
2024-07-08 01:00:58 +0300

    feat: Add DecksTypes.ts - types in a separate file

---
+++ src/services/decks/DecksTypes.ts
@@ -0,0 +1,41 @@
+export type Author = {
+  id: string
+  name: string
+}
+export type Deck = {
+  author: Author
+  cardsCount: number
+  cover: null | string
+  created: string
+  id: string
+  isFavorite: boolean
+  isPrivate: boolean
+  name: string
+  updated: string
+  userId: string
+}
+export type Pagination = {
+  currentPage: number
+  itemsPerPage: number
+  totalItems: number
+  totalPages: number
+}
+export type DecksResponse = {
+  items: Deck[]
+  pagination: Pagination
+}
+
+export type GetDecksArgs = {
+  authorId?: string
+  currentPage?: number
+  favoritedBy?: string
+  itemsPerPage?: number
+  maxCardsCount?: number
+  minCardsCount?: number
+  name?: string
+}
+
+export type CreateDeckArgs = {
+  isPrivate?: boolean
+  name: string
+}
