0ac60e179d99e76f7800bb3dc79bce9d7f5c8bea
k2 <izmail.k2@gmail.com>
2024-07-08 00:56:39 +0300

    feat: add store

---
+++ src/services/store.ts
@@ -0,0 +1,10 @@
+import { flashcardsApi } from '@/services/flashcardsApi'
+import { configureStore } from '@reduxjs/toolkit'
+
+export const store = configureStore({
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(flashcardsApi.middleware),
+  reducer: { [flashcardsApi.reducerPath]: flashcardsApi.reducer },
+})
+
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
