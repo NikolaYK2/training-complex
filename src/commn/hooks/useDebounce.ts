13f871bfd81a42aa29c10b36839e9e8ae49ab3b9
k2 <izmail.k2@gmail.com>
2024-07-08 00:56:01 +0300

    temp: refactor

--- src/commn/hooks/useDebounce.ts
+++ src/commn/hooks/useDebounce.ts
@@ -5,14 +5,26 @@ import { ChangeEvent, useEffect, useState } from 'react'
  * @param initialValue Начальное значение для дебаунса.
  * @param delay Задержка в миллисекундах перед вызовом коллбэка.
  * @param callback Функция обратного вызова, которая будет вызвана с дебаунсированным значением.
+ * @param onChange
  * @returns Объект с функциями handleChange, reset и текущим дебаунсированным значением.
  */
 export const useDebounce = (
   initialValue: string,
   delay: number,
-  callback?: (value: string) => void
+  callback?: (value: string) => void,
+  onChange?: (value: ChangeEvent<HTMLInputElement>) => void
 ) => {
   const [valueDebounce, setDebounce] = useState(initialValue)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setDebounce(e.currentTarget.value)
+    onChange?.(e)
+  }
+
+  // Функция для сброса текущего значения дебаунса
+  const reset = () => {
+    setDebounce('') // Сбрасываем значение дебаунса
+  }
 
   useEffect(() => {
     const handler = setTimeout(() => {
@@ -24,15 +36,12 @@ export const useDebounce = (
     }
   }, [valueDebounce])
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setDebounce(e.currentTarget.value)
-  }
-
-  // Функция для сброса текущего значения дебаунса
-  const reset = () => {
-    setDebounce('') // Сбрасываем значение дебаунса
-    callback && callback('') // Вызываем коллбэк с пустой строкой
-  }
+  useEffect(() => {
+    // Сбросить значение дебаунса при изменении initialValue
+    if (initialValue !== valueDebounce) {
+      setDebounce(initialValue || '') // Если initialValue пустое, устанавливаем ''
+    }
+  }, [initialValue])
 
   // Возвращаем объект с функциями и текущим значением дебаунса
   return { handleChange, reset, valueDebounce }
