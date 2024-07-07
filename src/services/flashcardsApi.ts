b94c90422cf9811926627bb1f30ca551a139f36a
k2 <izmail.k2@gmail.com>
2024-07-08 00:59:18 +0300

    feat: add flashcardsApi.ts

---
+++ src/services/flashcardsApi.ts
@@ -0,0 +1,17 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+export const flashcardsApi = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: 'https://api.flashcards.andrii.es',
+    credentials: 'include',
+    prepareHeaders: headers => {
+      headers.append('x-auth-skip', 'true')
+
+      return headers
+    },
+  }),
+  //можно эндпоинты разделять как бы на сущности
+  endpoints: () => ({}), //нужно указат ьпустой эндпооинт обязательно
+  reducerPath: 'flashcardsApi',
+  tagTypes: ['Decks'], //инвалидируем
+})
