6840bf7a546945b7503725b0f4c060564cec9c6b
k2 <izmail.k2@gmail.com>
2024-07-08 23:40:52 +0300

    feat: the first endpoint of the client registration request is implemented - Add error handling

--- src/features/auth/registration/SignUp.tsx
+++ src/features/auth/registration/SignUp.tsx
@@ -2,7 +2,9 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
+import { errorsResponse } from '@/commn/utils/errorsResponse'
 import { Page } from '@/features/pages/Page'
+import { useRegistrationAuthMutation } from '@/services/auth/authService'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
@@ -19,6 +21,14 @@ export const RegisterSchema = z
 
 export type RegisterFormType = z.infer<typeof RegisterSchema>
 
+const emailConfirmationTemplate = `
+  <b>Hello, ##name##!</b><br/>
+  Please confirm your email by clicking on the link below:<br/>
+  <a href="http://localhost:5173/confirm-email/##token##">Confirm email</a>.
+  If it doesn't work, copy and paste the following link in your browser:<br/>
+  http://localhost:5173/confirm-email/  ##token##
+`
+
 export const SignUp = () => {
   const {
     control,
@@ -33,9 +43,25 @@ export const SignUp = () => {
     },
     resolver: zodResolver(RegisterSchema),
   })
+  const [registration, { error, isError, isLoading: isLoadingRegistration }] =
+    useRegistrationAuthMutation()
+
   const onSubmit: SubmitHandler<RegisterFormType> = data => {
-    console.log(data)
+    registration({
+      email: data.email,
+      html: emailConfirmationTemplate,
+      password: data.password,
+      sendConfirmationEmail: true,
+    })
     reset()
+  }
+
+  if (isLoadingRegistration) {
+    return <div>Loading...</div>
+  }
+
+  if (isError) {
+    return <div>{errorsResponse(error)}</div>
   }
 
   return (
