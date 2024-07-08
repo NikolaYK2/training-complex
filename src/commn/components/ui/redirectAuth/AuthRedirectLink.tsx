95841ac58d849697356f8dd51086fe482b449922
k2 <izmail.k2@gmail.com>
2024-07-08 17:51:11 +0300

    refactor: Button now accepts link for navigation by routes

--- src/commn/components/ui/redirectAuth/AuthRedirectLink.tsx
+++ src/commn/components/ui/redirectAuth/AuthRedirectLink.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom'
+
 import { Button } from '@/commn/components/ui/button'
 import { TitleType } from '@/commn/components/ui/formAuth/FormAuth'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
@@ -16,7 +18,12 @@ export const AuthRedirectLink = ({ className, describe = false, title }: Props) 
   return (
     <div className={className}>
       {describe && <TextFormat variant={'body2'}>{getTitle(title).message}</TextFormat>}
-      <Button as={'a'} style={{ textTransform: 'capitalize' }} variant={'link'}>
+      <Button
+        as={Link}
+        style={{ textDecoration: describe ? 'underline' : '', textTransform: 'capitalize' }}
+        to={getTitle(title).route}
+        variant={'link'}
+      >
         {getTitle(title).buttonName}
       </Button>
     </div>
@@ -26,13 +33,23 @@ export const AuthRedirectLink = ({ className, describe = false, title }: Props) 
 const getTitle = (title: TitleType): RoutesType => {
   switch (title) {
     case 'sign in': {
-      return { buttonName: 'sign un', message: "Don't have an account?", route: 'sign-un' }
+      return { buttonName: 'sign un', message: "Don't have an account?", route: '/register' }
     }
     case 'sign up': {
-      return { buttonName: 'sign in', message: 'Already have an account?', route: 'sign-in' }
+      return { buttonName: 'sign in', message: 'Already have an account?', route: '/login' }
     }
     case 'forgot your password?': {
-      return { buttonName: 'Try logging in', message: 'Already have an account?', route: 'sign-in' }
+      return {
+        buttonName: 'Try logging in',
+        message: 'Did you remember your password?',
+        route: '/login',
+      }
+    }
+    case 'forgot password': {
+      return {
+        buttonName: 'Forgot Password?',
+        route: '/forgot-password',
+      }
     }
 
     default: {
