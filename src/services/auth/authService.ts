88ed99506e3168ed84ea9a3f06caa33f8a3850cd
k2 <izmail.k2@gmail.com>
2024-07-11 13:29:59 +0300

    feat: add getVerifyEmail

--- src/services/auth/authService.ts
+++ src/services/auth/authService.ts
@@ -1,18 +1,25 @@
 import { RegistrationArgs, RegistrationResponse } from '@/services/auth/AuthTypes'
 import { flashcardsApi } from '@/services/flashcardsApi'
 
-const AUTH = 'v1/auth/sign-up'
+const AUTH = 'v1/auth/'
 
 export const authService = flashcardsApi.injectEndpoints({
   endpoints: builder => ({
+    getVerifyEmail: builder.mutation<void, { token: string }>({
+      query: token => ({
+        body: token,
+        method: 'POST',
+        url: `${AUTH}verify-email`,
+      }),
+    }),
     registrationAuth: builder.mutation<RegistrationResponse, RegistrationArgs>({
       query: args => ({
         body: args,
         method: 'POST',
-        url: AUTH,
+        url: `${AUTH}sign-up`,
       }),
     }),
   }),
 })
 
-export const { useRegistrationAuthMutation } = authService
+export const { useGetVerifyEmailMutation, useRegistrationAuthMutation } = authService
