ad4c77b5f3ddeabbd276fc08a9fac5abfa86fdb0
k2 <izmail.k2@gmail.com>
2024-07-11 22:03:34 +0300

    refactor: add prop response

--- src/features/auth/confirmEmail/ConfirmEmail.tsx
+++ src/features/auth/confirmEmail/ConfirmEmail.tsx
@@ -27,7 +27,7 @@ export const ConfirmEmail = () => {
         <Loading />
       ) : (
         <Card>
-          <FormAuth title={'confirm email'} />
+          <FormAuth response={'Email verified successfully'} title={'confirm email'} />
         </Card>
       )}
     </Page>
