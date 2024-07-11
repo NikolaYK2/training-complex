50ac20a4f14e5616e2e02a4acffa1a05aab5a294
k2 <izmail.k2@gmail.com>
2024-07-11 21:50:06 +0300

    feat: get the email parameter from the given useNavigate parameter

--- src/features/auth/checkEmail/CheckEmail.tsx
+++ src/features/auth/checkEmail/CheckEmail.tsx
@@ -1,12 +1,20 @@
+import { useLocation } from 'react-router-dom'
+
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
 import { Page } from '@/features/pages/Page'
 
 export const CheckEmail = () => {
+  const location = useLocation()
+  const { email } = location.state || {}
+
   return (
     <Page marginTop={'var(--margin-top-page)'}>
       <Card>
-        <FormAuth title={'check email'} />
+        <FormAuth
+          response={`We’ve sent an Email with instructions to ${email}`}
+          title={'check email'}
+        />
       </Card>
     </Page>
   )
