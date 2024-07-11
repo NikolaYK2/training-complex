a5ebbeef49c66912f00a2b030f04c4ee032c2f63
k2 <izmail.k2@gmail.com>
2024-07-11 22:03:04 +0300

    refactor: remove space

--- src/features/auth/registration/SignUp.tsx
+++ src/features/auth/registration/SignUp.tsx
@@ -28,7 +28,7 @@ const emailConfirmationTemplate = `
   Please confirm your email by clicking on the link below:<br/>
   <a href="http://localhost:5173/confirm-email/##token##">Confirm email</a>.
   If it doesn't work, copy and paste the following link in your browser:<br/>
-  http://localhost:5173/confirm-email/  ##token##
+  http://localhost:5173/confirm-email/##token##
 `
 
 export const SignUp = () => {
