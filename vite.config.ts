8dec7f5b5870982831c32825144dee87e3c76cff
k2 <izmail.k2@gmail.com>
2024-07-12 14:11:23 +0300

    build: add additionalData option for SCSS preprocessor
    - Added the additionalData option to the SCSS preprocessor configuration in the build process. This option imports all mixins from '@/assets/styles/_mixins.scss' into every SCSS file.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,13 @@ import { defineConfig } from 'vite'
 // https://vitejs.dev/config/
 
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use '@/assets/styles/_mixins.scss' as *;`,
+      },
+    },
+  },
   plugins: [react()],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
