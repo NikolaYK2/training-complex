3a45f056509b849871081eeaf4fbc004c42e5fa9
k2 <izmail.k2@gmail.com>
2024-07-12 14:18:09 +0300

    feat: submission form template placed in a separate file for use

---
+++ src/features/auth/emailTemplate/emailTemplate.ts
@@ -0,0 +1,7 @@
+export const emailConfirmationTemplate = `
+  <b>Hello, ##name##!</b><br/>
+  Please confirm your email by clicking on the link below:<br/>
+  <a href="http://localhost:5173/confirm-email/##token##">Confirm email</a>.
+  If it doesn't work, copy and paste the following link in your browser:<br/>
+  http://localhost:5173/confirm-email/##token##
+`
