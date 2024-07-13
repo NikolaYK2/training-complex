2440e6d97ab733aa909610f6108d0dfec9636a83
k2 <izmail.k2@gmail.com>
2024-07-13 23:33:03 +0300

    feat: Implement password reset functionality
    - useParams to retrieve the token from the URL.
    - useNavigate for programmatic navigation after password reset.
    - async/await for handling form submission.
    - Display loading indicator during password reset process.
    - errors gracefully and display error message if reset fails.
    - Add setTimeout to delay navigation to login page after successful password reset.

--- src/features/auth/createPassword/CreateNewPassword.tsx
+++ src/features/auth/createPassword/CreateNewPassword.tsx
@@ -1,7 +1,11 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { useNavigate, useParams } from 'react-router-dom'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
+import { Loading } from '@/commn/components/ui/loading/Loading'
+import { LOGIN } from '@/routes/Router'
+import { usePasswordResetMutation } from '@/services/auth/authService'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
@@ -22,16 +26,36 @@ export const CreateNewPassword = () => {
     },
     resolver: zodResolver(CreateNewPasswordSchema),
   })
-  const onSubmit: SubmitHandler<CreateNewPasswordType> = data => {
-    console.log(data)
-    reset()
+  const { token } = useParams<{ token: string }>()
+  const navigate = useNavigate()
+  const [resetPassword, { error, isError, isLoading }] = usePasswordResetMutation()
+  const onSubmit: SubmitHandler<CreateNewPasswordType> = async data => {
+    try {
+      if (token) {
+        await resetPassword({ password: data.password, token })
+        reset()
+        // Добавление задержки или других мер подтверждения успешного сброса пароля
+        // перед переходом на страницу входа
+        setTimeout(() => {
+          navigate(LOGIN)
+        }, 1000)
+      }
+    } catch (e) {
+      console.error('New password ', e)
+    }
+  }
+
+  if (isError) {
+    return <div>Error: {JSON.stringify(error)}</div>
   }
 
   return (
     <Card>
+      {isLoading && <Loading />}
       <FormAuth
         control={control}
         errorMessage={errors}
+        formItem={['password']}
         onSubmit={handleSubmit(onSubmit)}
         title={'create new password'}
       />
