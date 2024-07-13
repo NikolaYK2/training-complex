956f5a6a1aa1ed37c4f4522d5b382e92aea3f0be
k2 <izmail.k2@gmail.com>
2024-07-13 13:29:00 +0300

    feat: add permission - add error handling - add download - save

--- src/features/auth/login/SignIn.tsx
+++ src/features/auth/login/SignIn.tsx
@@ -2,14 +2,17 @@ import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
+import { Loading } from '@/commn/components/ui/loading/Loading'
+import { localStorageUtil } from '@/commn/utils/localStorageUtil'
 import { Page } from '@/features/pages/Page'
+import { useLoginAuthMutation } from '@/services/auth/authService'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().trim().email(),
   password: z.string().min(5),
-  remember: z.boolean().optional(),
+  remember: z.boolean().default(false), // Добавить default значение,
 })
 
 export type LoginFormType = z.infer<typeof loginSchema>
@@ -29,14 +32,33 @@ export const SignIn = () => {
     resolver: zodResolver(loginSchema),
   })
 
-  const onSubmit: SubmitHandler<LoginFormType> = data => {
-    console.log(data)
-    reset()
+  const [authorisation, { error, isError, isLoading }] = useLoginAuthMutation()
+
+  const onSubmit: SubmitHandler<LoginFormType> = async data => {
+    try {
+      const tokensRes = await authorisation({
+        email: data.email,
+        password: data.password,
+        rememberMe: data.remember,
+      }).unwrap()
+
+      localStorageUtil.saveItem('accessToken', tokensRes?.accessToken)
+      localStorageUtil.saveItem('refreshToken', tokensRes?.refreshToken)
+
+      reset()
+    } catch (e) {
+      console.error('login error: ', e)
+    }
+  }
+
+  if (isError) {
+    return <div>{JSON.stringify(error)}</div>
   }
 
   return (
     <Page marginTop={'var(--margin-top-page)'}>
       <Card>
+        {isLoading && <Loading />}
         <FormAuth
           control={control}
           errorMessage={errors}
