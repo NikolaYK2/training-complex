9e17c2b4b06b06ecbb9c2a5bf152e7a563c61440
k2 <izmail.k2@gmail.com>
2024-07-13 17:51:52 +0300

    feat: add templatesEmail.ts

--- src/features/auth/emailTemplate/emailTemplate.ts
+++
@@ -1,7 +0,0 @@
-export const emailConfirmationTemplate = `
-  <b>Hello, ##name##!</b><br/>
-  Please confirm your email by clicking on the link below:<br/>
-  <a href="http://localhost:5173/confirm-email/##token##">Confirm email</a>.
-  If it doesn't work, copy and paste the following link in your browser:<br/>
-  http://localhost:5173/confirm-email/##token##
-`
---
+++ src/features/auth/templates/templatesEmail.ts
@@ -0,0 +1,15 @@
+type TemplatesAuthType = 'checkEmail' | 'recoverPassword'
+
+export const templatesEmail: Record<TemplatesAuthType, string> = {
+  checkEmail: `
+  <b>Hello, ##name##!</b><br/>
+  Please confirm your email by clicking on the link below:<br/>
+  <a href="http://localhost:5173/confirm-email/##token##">Confirm email</a>.
+  If it doesn't work, copy and paste the following link in your browser:<br/>
+  http://localhost:5173/confirm-email/##token##
+`,
+  recoverPassword: `
+  <h1>Hi, ##name##</h1>
+  <h1>Hi, ##name##</h1><p>Click <a href="http://localhost:5173/create-password/##token##">here</a> to recover your password</p>
+`,
+}
