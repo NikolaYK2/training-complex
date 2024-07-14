1bcd3c77e03b0ee427e583ce1b773593342efb59
k2 <izmail.k2@gmail.com>
2024-07-14 18:03:29 +0300

    temp: Add temporary display condition when logging in to an account feat: add DropMenu for working with profile

--- src/features/1-header/Header.tsx
+++ src/features/1-header/Header.tsx
@@ -1,22 +1,40 @@
 import { Link } from 'react-router-dom'
 
 import { Button } from '@/commn/components/ui/button'
+import { DropDownMenu } from '@/commn/components/ui/dropDownMenu/DropDownMenu'
 import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+import { useGetCurrentUserDataQuery, useLogoutMutation } from '@/services/auth/authService'
 
 import s from './Header.module.scss'
 
 export const Header = () => {
+  const { data, error, isError, isLoading } = useGetCurrentUserDataQuery()
+  const [logout, { error: errorLogout, isError: isErrorLogout }] = useLogoutMutation()
+  const isLogin = false
+
   return (
     <header className={s.header}>
       <div className={s.container}>
         <div className={s.logo}>
           <IconSvg name={'logo'} />
         </div>
-
-        <Button as={Link} to={'/login'} variant={'secondary'}>
-          <TextFormat variant={'subtitle2'}>Sign In</TextFormat>
-        </Button>
+        {isLogin ? (
+          <Button as={Link} to={'/login'} variant={'secondary'}>
+            <TextFormat variant={'subtitle2'}>Sign In</TextFormat>
+          </Button>
+        ) : (
+          <DropDownMenu
+            menuConfig={{
+              content: [
+                { buttonName: 'Ivan', email: 'emae', icon: 'avatar' },
+                { buttonName: 'My profile', className: 'iconProfile', icon: 'profile' },
+                { buttonName: 'sign out', className: 'iconLogOut', icon: 'logOut' },
+              ],
+              trigger: 'Ivan',
+            }}
+          ></DropDownMenu>
+        )}
       </div>
     </header>
   )
