e0c215fbc4f9a772c979484ec244896b47b92226
k2 <izmail.k2@gmail.com>
2024-07-15 12:54:42 +0300

    feat: add prop for route

--- src/commn/components/ui/dropDownMenu/DropDownMenu.tsx
+++ src/commn/components/ui/dropDownMenu/DropDownMenu.tsx
@@ -1,3 +1,6 @@
+import { Link } from 'react-router-dom'
+
+import { Button } from '@/commn/components/ui/button'
 import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
@@ -5,9 +8,11 @@ import s from './DropDownMenu.module.scss'
 
 export type MenuItem = {
   buttonName: string
+  callback?: () => void
   className?: string
   email?: string
   icon: 'avatar' | 'delete' | 'edit' | 'learn' | 'logOut' | 'profile'
+  route?: string
 }
 type MenuType = {
   content: MenuItem[]
@@ -41,18 +46,23 @@ export const DropDownMenu = ({ menuConfig }: Props) => {
         <DropdownMenu.Content className={s.content} sideOffset={5}>
           {menuConfig.content.map(el => (
             <DropdownMenu.Item
-              className={`${s.item} ${!el.email ? s.isActive : ''} ${
-                el.className ? el.className : ''
-              }`}
+              className={`${s.item} ${el.className ? el.className : ''}`}
               key={el.icon}
             >
-              <div className={`${el.email ? s.avatar : s.iconSlot}`}>
-                <IconSvg name={el.icon} />
-              </div>
-              <div className={`${s.text}`}>
-                <span>{el.buttonName}</span>
-                <span>{el.email}</span>
-              </div>
+              <Button
+                as={el.route ? Link : 'button'}
+                className={s.block}
+                onClick={el.callback}
+                to={el.route ?? ''}
+              >
+                <div className={`${el.email ? s.avatar : s.iconSlot}`}>
+                  <IconSvg name={el.icon} />
+                </div>
+                <div className={`${s.text}`}>
+                  <span>{el.buttonName}</span>
+                  <span>{el.email}</span>
+                </div>
+              </Button>
             </DropdownMenu.Item>
           ))}
           <DropdownMenu.Arrow className={s.menuArrow} />
