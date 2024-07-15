f3f349c14cd8020240d194edc8cd7a87aefe7c36
k2 <izmail.k2@gmail.com>
2024-07-15 21:40:12 +0300

    refactor: isEditingPersonalInfo state moved here - implemented updating the name form and logging out of the account

--- src/features/auth/personalInformation/PersonalInformation.tsx
+++ src/features/auth/personalInformation/PersonalInformation.tsx
@@ -1,11 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { SubmitHandler, useForm } from 'react-hook-form'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
 import { Loading } from '@/commn/components/ui/loading/Loading'
 import { Page } from '@/features/pages/Page'
-import { useGetCurrentUserDataQuery, useUpdateUserDataMutation } from '@/services/auth/authService'
+import {
+  useGetCurrentUserDataQuery,
+  useLogoutMutation,
+  useUpdateUserDataMutation,
+} from '@/services/auth/authService'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
@@ -16,11 +20,15 @@ export const personalInformationSchema = z.object({
 export type PersonalInformationType = z.infer<typeof personalInformationSchema>
 
 export const PersonalInformation = () => {
+  const [isEditingPersonalInfo, setIsEditingPersonalInfo] = useState(false)
+
   const { data, error, isError, isLoading } = useGetCurrentUserDataQuery()
   const [
     updateUserData,
     { error: errorUpdUser, isError: isErrorUpdUser, isLoading: isLoadingUpdUser },
   ] = useUpdateUserDataMutation()
+  const [logOut, { error: errorLogout, isError: isErrorLogout, isLoading: isLoadingLogout }] =
+    useLogoutMutation()
   const {
     control,
     formState: { errors },
@@ -35,13 +43,17 @@ export const PersonalInformation = () => {
 
   const onSubmit: SubmitHandler<PersonalInformationType> = async data => {
     try {
-      const formData = new FormData()
+      if (isEditingPersonalInfo) {
+        const formData = new FormData()
 
-      formData.append('name', data.text)
-
-      await updateUserData(formData)
+        formData.append('name', data.text)
+        await updateUserData(formData)
+        setIsEditingPersonalInfo(false)
+      } else {
+        await logOut()
+      }
     } catch (e) {
-      console.error('upd Text user data ', e)
+      console.error('Error: ', e)
     }
   }
 
@@ -51,26 +63,23 @@ export const PersonalInformation = () => {
     }
   }, [data, setValue])
 
-  if (isErrorUpdUser || isError) {
-    if (error) {
-      return <div>Error: {JSON.stringify(error)}</div>
-    }
-    if (errorUpdUser) {
-      return <div>Error: {JSON.stringify(errorUpdUser)}</div>
-    }
+  if (isErrorUpdUser || isError || isErrorLogout) {
+    return <div>Error: {JSON.stringify(error || errorUpdUser || errorLogout)}</div>
   }
 
   return (
     <Page marginTop={'var(--margin-top-page)'}>
       <Card>
-        {isLoading || isLoadingUpdUser ? (
+        {isLoading || isLoadingUpdUser || isLoadingLogout ? (
           <Loading />
         ) : (
           <FormAuth
             control={control}
             errorMessage={errors}
             formItem={['text']}
+            isEditingPersonalInfo={isEditingPersonalInfo}
             onSubmit={handleSubmit(onSubmit)}
+            setIsEditingPersonalInfo={setIsEditingPersonalInfo}
             title={'personal information'}
           />
         )}
