ede6b275ab136b41499c6fe81a83ee3ada0e82fb
k2 <izmail.k2@gmail.com>
2024-07-15 12:57:46 +0300

    refactor: handle errors and loading states for logout and user data fetching
    - Displayed appropriate error messages when `isError` or `isErrorLogout` is true
    - Displayed loading indicator when `isLoading` or `isLoadingLogout` is true
    - Configured DropDownMenu with user data and logout functionality

--- src/features/1-header/Header.tsx
+++ src/features/1-header/Header.tsx
@@ -3,37 +3,66 @@ import { Link } from 'react-router-dom'
 import { Button } from '@/commn/components/ui/button'
 import { DropDownMenu } from '@/commn/components/ui/dropDownMenu/DropDownMenu'
 import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
+import { Loading } from '@/commn/components/ui/loading/Loading'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+import { HOME_ROUTE, LOGIN_ROUTE, PROFILE_ROUTE } from '@/routes/Router'
 import { useGetCurrentUserDataQuery, useLogoutMutation } from '@/services/auth/authService'
 
 import s from './Header.module.scss'
 
 export const Header = () => {
   const { data, error, isError, isLoading } = useGetCurrentUserDataQuery()
-  const [logout, { error: errorLogout, isError: isErrorLogout }] = useLogoutMutation()
+  const [logout, { error: errorLogout, isError: isErrorLogout, isLoading: isLoadingLogout }] =
+    useLogoutMutation()
   const isLogin = false
+
+  if (isError) {
+    return <div>Error: {JSON.stringify(error)}</div>
+  }
+  if (isErrorLogout) {
+    return <div>Error: {JSON.stringify(errorLogout)}</div>
+  }
+
+  if (isLoading) {
+    return <Loading />
+  }
+  if (isLoadingLogout) {
+    return <Loading />
+  }
 
   return (
     <header className={s.header}>
       <div className={s.container}>
-        <div className={s.logo}>
+        <Link className={s.logo} to={HOME_ROUTE}>
           <IconSvg name={'logo'} />
-        </div>
+        </Link>
         {isLogin ? (
-          <Button as={Link} to={'/login'} variant={'secondary'}>
+          <Button as={Link} to={LOGIN_ROUTE} variant={'secondary'}>
             <TextFormat variant={'subtitle2'}>Sign In</TextFormat>
           </Button>
         ) : (
-          <DropDownMenu
-            menuConfig={{
-              content: [
-                { buttonName: 'Ivan', email: 'emae', icon: 'avatar' },
-                { buttonName: 'My profile', className: 'iconProfile', icon: 'profile' },
-                { buttonName: 'sign out', className: 'iconLogOut', icon: 'logOut' },
-              ],
-              trigger: 'Ivan',
-            }}
-          ></DropDownMenu>
+          <>
+            <DropDownMenu
+              menuConfig={{
+                content: [
+                  { buttonName: data?.name ?? '', email: data?.email, icon: 'avatar' },
+                  {
+                    buttonName: 'My profile',
+                    className: 'iconProfile',
+                    icon: 'profile',
+                    route: PROFILE_ROUTE,
+                  },
+                  {
+                    buttonName: 'sign out',
+                    callback: logout,
+                    className: 'iconLogOut',
+                    icon: 'logOut',
+                  },
+                ],
+                trigger: data?.name ?? '',
+              }}
+            ></DropDownMenu>
+          </>
         )}
       </div>
     </header>
