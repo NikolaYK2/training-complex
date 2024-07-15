7fc2a5bff1cb4d329a7959e4f5b7d8a5a23fac8c
k2 <izmail.k2@gmail.com>
2024-07-15 19:11:56 +0300

    feat: tagType for service auth

--- src/services/flashcardsApi.ts
+++ src/services/flashcardsApi.ts
@@ -13,5 +13,5 @@ export const flashcardsApi = createApi({
   //можно эндпоинты разделять как бы на сущности
   endpoints: () => ({}), //нужно указат ьпустой эндпооинт обязательно
   reducerPath: 'flashcardsApi',
-  tagTypes: ['Decks'], //инвалидируем
+  tagTypes: ['Decks', 'Auth'], //инвалидируем
 })
