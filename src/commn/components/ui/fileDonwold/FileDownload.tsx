34bdbc9ae4050504c917d6b0aaadaa807f19ec84
k2 <izmail.k2@gmail.com>
2024-07-18 14:34:30 +0300

    feat: add FileDownload.tsx

---
+++ src/commn/components/ui/fileDonwold/FileDownload.tsx
@@ -0,0 +1,51 @@
+import { ChangeEvent, ReactElement } from 'react'
+
+import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+
+import s from './FileDownload.module.scss'
+
+type Props = {
+  buttonName?: string
+  callback: (value: FormData) => void
+  className?: string
+  disabled?: boolean
+  iconComponent: ReactElement
+}
+export const FileDownload = ({
+  buttonName,
+  callback,
+  className,
+  disabled,
+  iconComponent,
+}: Props) => {
+  const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      const formData: FormData = new FormData()
+
+      formData.append('avatar', e.target.files[0])
+
+      try {
+        await callback?.(formData)
+      } catch (error) {
+        console.error('Error updating user data:', error)
+      }
+    }
+  }
+
+  return (
+    <label className={`${s.editAvatar} ${className || ''}`}>
+      <input
+        className={s.editInput}
+        disabled={disabled}
+        onChange={handleFileChange}
+        type={'file'}
+      />
+      {iconComponent}
+      {buttonName && (
+        <TextFormat style={{ textTransform: 'capitalize' }} variant={'subtitle2'}>
+          {buttonName}
+        </TextFormat>
+      )}
+    </label>
+  )
+}
