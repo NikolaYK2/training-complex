64261a49633d7d48462bd1b7412538a2361b8e68
k2 <izmail.k2@gmail.com>
2024-07-19 20:35:16 +0300

    refactor: integrate form and file upload functionality in CreateDeck component
    - Add useState hooks to manage file preview state
    - Integrate react-hook-form for form handling with zod validation
    - Create controlled components for file upload and text input
    - Implement DialogModal for deck creation with form submission
    - Handle form submission and reset state on close

---
+++ src/features/decks/createDeck/CreateDeck.tsx
@@ -0,0 +1,107 @@
+import { useState } from 'react'
+import { SubmitHandler, useForm } from 'react-hook-form'
+
+import { ImageIcon } from '@/assets/image/image/ImageIcon'
+import { TickBox } from '@/commn/components/ui/checkBox/TickBox'
+import { ControlledFileDownload } from '@/commn/components/ui/fileDonwold/ControlledFileDownload'
+import { FIlePreview } from '@/commn/components/ui/filePreview/FIlePreview'
+import { ControlledTextField } from '@/commn/components/ui/input/ControlledTextField'
+import { DialogModal } from '@/commn/components/ui/modals/dialog/DialogModal'
+import { useCreateDeckMutation } from '@/services/decks/decksService'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import s from './CreateDeck.module.scss'
+
+const createDeckSchema = z.object({
+  cover: z.any().optional().nullable(),
+  isPrivate: z.boolean().default(false),
+  name: z.string().trim().min(3, 'min 3 litters'),
+})
+
+type FormType = z.infer<typeof createDeckSchema>
+export const CreateDeck = () => {
+  const [filePreview, setFilePreview] = useState<null | string>(null) // Состояние для хранения URL изображения
+  const [filePreviewFullScreen, setFilePreviewFullScreen] = useState(false) // Состояние для хранения URL изображения
+  const [isOpenModal, setIsOpenModal] = useState(false)
+  const [createDeck, { error, isError, isLoading: isLoadingCreatedDeck }] = useCreateDeckMutation()
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    reset,
+  } = useForm<FormType>({
+    defaultValues: {
+      cover: null,
+      isPrivate: false,
+      name: '',
+    },
+    resolver: zodResolver(createDeckSchema),
+  })
+
+  // Очистка состояния при успешной отправке формы
+  const handleClose = () => {
+    setFilePreview(null) // Очистить изображение
+    setIsOpenModal(false) //закрытие модалки
+    setFilePreviewFullScreen(false)
+    reset() // Сброс формы
+  }
+  const handleCloseModal = () => {
+    setIsOpenModal(!isOpenModal) //закрытие модалки
+    setFilePreviewFullScreen(false)
+  }
+  const onSubmit: SubmitHandler<FormType> = async data => {
+    try {
+      // await createDeck({ isPrivate: data.isPrivate, name: data.name })
+      console.log(data)
+      handleClose()
+    } catch (e) {
+      console.error('Error creating deck: ', e)
+    }
+  }
+
+  if (isError) {
+    return <div>Error: {JSON.stringify(error)}</div>
+  }
+
+  return (
+    <DialogModal
+      isOpenModal={isOpenModal}
+      onSubmit={handleSubmit(onSubmit)}
+      setIsOpenModal={handleCloseModal}
+      textH2={'add new deck'}
+      trigger={'add new card'}
+    >
+      {[
+        <ControlledTextField
+          control={control}
+          errorMessage={errors.name?.message}
+          key={'input-name'}
+          label={'Name Pack'}
+          name={'name'}
+          type={'text'}
+        />,
+
+        <FIlePreview
+          filePreview={filePreview}
+          filePreviewFullScreen={filePreviewFullScreen}
+          key={'file-preview'}
+          setFilePreview={setFilePreview}
+          setFilePreviewFullScreen={setFilePreviewFullScreen}
+        />,
+
+        <ControlledFileDownload
+          buttonName={'upload image'}
+          className={s.newDeck}
+          control={control}
+          iconComponent={<ImageIcon />}
+          key={'input-file'}
+          name={'cover'}
+          setFilePreview={setFilePreview}
+        />,
+        <TickBox key={'TickBox'} label={'opana!'} />,
+      ]}
+    </DialogModal>
+  )
+}
