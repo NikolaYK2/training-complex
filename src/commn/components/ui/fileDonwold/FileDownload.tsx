a5f6c6853129be5ca004b3d8c7989167a1f8b740
k2 <izmail.k2@gmail.com>
2024-07-19 20:17:43 +0300

    refactor: add props - in fn handleFileChange add write image to state for preview - component wrapped in forwardRef - add state key for forced update component

--- src/commn/components/ui/fileDonwold/FileDownload.tsx
+++ src/commn/components/ui/fileDonwold/FileDownload.tsx
@@ -1,37 +1,67 @@
-import { ChangeEvent, ReactElement } from 'react'
+import { ChangeEvent, ReactElement, forwardRef, useState } from 'react'
 
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 
 import s from './FileDownload.module.scss'
 
-type Props = {
+export type FiledDownloadProps = {
   buttonName?: string
-  callback: (value: FormData) => void
   className?: string
   disabled?: boolean
   iconComponent: ReactElement
+  name: string
+  onChange: (file: FormData) => void
+  setFilePreview?: (file: null | string) => void
 }
 
-export const FileDownload = (props: Props) => {
-  const { buttonName, callback, className, disabled, iconComponent } = props
+export const FileDownload = forwardRef<HTMLInputElement, FiledDownloadProps>((props, ref) => {
+  const {
+    buttonName,
+    className,
+    disabled,
+    iconComponent,
+    name,
+    onChange,
+    setFilePreview,
+    ...rest
+  } = props
+  const [fileInputKey, setFileInputKey] = useState(0)
 
+  // Обработчик изменения файла
   const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
     if (e.target.files && e.target.files.length > 0) {
-      const formData: FormData = new FormData()
+      const formData: FormData = new FormData() // Создание нового FormData
+      const file = e.target.files[0] // Получение выбранного файла
 
-      formData.append('avatar', e.target.files[0])
+      formData.append(name, file) // Добавление файла в FormData
 
-      try {
-        await callback?.(formData)
-      } catch (error) {
-        console.error('Error updating user data:', error)
+      if (file) {
+        //Создание FileReader для чтения файла и создания URL
+        const reader = new FileReader()
+
+        reader.onloadend = () => {
+          setFilePreview?.(reader.result as string) // Сохранение URL в состоянии
+          setFileInputKey(prev => prev + 1) //для предотвращения кеширования браузером, в случаи повторной загрузки той же картинки
+        }
+        reader.readAsDataURL(file) // Чтение файла как Data URL
       }
+
+      // Вызов коллбэка onChange с FormData
+      onChange?.(formData)
     }
   }
 
   return (
     <label className={`${s.editAvatar} ${className || ''}`}>
-      <input className={s.input} disabled={disabled} onChange={handleFileChange} type={'file'} />
+      <input
+        className={s.input}
+        disabled={disabled}
+        key={fileInputKey}
+        onChange={handleFileChange}
+        ref={ref}
+        type={'file'}
+        {...rest}
+      />
       {iconComponent}
       {buttonName && (
         <TextFormat style={{ textTransform: 'capitalize' }} variant={'subtitle2'}>
@@ -40,4 +70,4 @@ export const FileDownload = (props: Props) => {
       )}
     </label>
   )
-}
+})
