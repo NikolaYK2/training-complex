1a31ee2e6ca76c48dcaa305d3dfc8883f551c017
k2 <izmail.k2@gmail.com>
2024-07-19 20:33:20 +0300

    feat: add FullscreenIcon.tsx

---
+++ src/assets/image/fuuscreen/FullscreenIcon.tsx
@@ -0,0 +1,18 @@
+import s from './FullscreenIcon.module.scss'
+
+export const FullscreenIcon = () => {
+  return (
+    <div className={s.containerFullscreenIcon}>
+      <svg
+        height={'100%'}
+        viewBox={'0 0 32 32'}
+        width={'100%'}
+        xmlns={'http://www.w3.org/2000/svg'}
+      >
+        <path
+          d={'M4 4v9h2V6h7V4H4zm15 0v2h7v7h2V4h-9zM4 19v9h9v-2H6v-7H4zm22 0v7h-7v2h9v-9h-2z'}
+        ></path>
+      </svg>
+    </div>
+  )
+}
