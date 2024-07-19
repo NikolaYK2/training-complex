d9526b8440de394fac2b0cb01b96b421962857c6
k2 <izmail.k2@gmail.com>
2024-07-19 17:48:29 +0300

    feat: add ControlledFileDownload.tsx

---
+++ src/commn/components/ui/fileDonwold/ControlledFileDownload.tsx
@@ -0,0 +1,33 @@
+import { FieldValues, useController } from 'react-hook-form'
+
+import { FileDownload, FiledDownloadProps } from '@/commn/components/ui/fileDonwold/FileDownload'
+import { UseControllerProps } from '@/commn/typs/UseControllerProps'
+
+export type ControlledTextFieldProps<TFieldValues extends FieldValues> =
+  UseControllerProps<TFieldValues> & Omit<FiledDownloadProps, 'id' | 'onChange' | 'value'>
+
+export const ControlledFileDownload = <TFieldValues extends FieldValues>({
+  buttonName,
+  control,
+  name,
+  ...fileDownloadProps
+}: ControlledTextFieldProps<TFieldValues>) => {
+  // Используем хук useController для получения необходимых свойств
+  const {
+    field: { onChange, ref },
+  } = useController({ control, name })
+  // Обработчик изменения файла
+  const handleFileChange = (file: FormData) => {
+    onChange(file)
+  }
+
+  return (
+    <FileDownload
+      {...fileDownloadProps}
+      buttonName={buttonName}
+      name={name}
+      onChange={handleFileChange}
+      ref={ref}
+    />
+  )
+}
