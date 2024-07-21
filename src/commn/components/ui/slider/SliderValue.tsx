c0ba9ef7575e40b5fa3a160c2bd44225e1e78ecc
k2 <izmail.k2@gmail.com>
2024-07-21 16:37:20 +0300

    Refactor: add debounce function - Add props for control

--- src/commn/components/ui/slider/SliderValue.tsx
+++ src/commn/components/ui/slider/SliderValue.tsx
@@ -1,45 +1,77 @@
-import { ChangeEvent, useState } from 'react'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
 
+import { debounce } from '@/commn/utils/debounce'
 import * as Slider from '@radix-ui/react-slider'
 
 import s from './SliderValue.module.scss'
 
-export const SliderValue = () => {
-  const [valueNum1, setValueNum1] = useState(19)
-  const [valueNum2, setValueNum2] = useState(79)
+type Props = {
+  maxValue: number
+  minValue: number
+  setCountMaxDecks: (countMax: number) => void
+  setCountMinDecks: (countMin: number) => void
+}
+
+export const SliderValue = ({
+  maxValue: max,
+  minValue: min,
+  setCountMaxDecks,
+  setCountMinDecks,
+}: Props) => {
+  const [minValue, setMinValue] = useState(min)
+  const [maxValue, setMaxValue] = useState(max)
+
+  const debouncedUpdate = useRef(
+    debounce((minVal: number, maxVal: number) => {
+      setCountMinDecks(minVal)
+      setCountMaxDecks(maxVal)
+    }, 500)
+  ).current
 
   const changeSliderValues = (value: number[]) => {
-    setValueNum1(value[0])
-    setValueNum2(value[1])
+    setMinValue(value[0])
+    setMaxValue(value[1])
+    debouncedUpdate(value[0], value[1])
   }
 
-  const changeValue1 = (e: ChangeEvent<HTMLInputElement>) => {
-    setValueNum1(parseInt(e.currentTarget.value))
+  const changeMinValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.currentTarget.value, 10)
+
+    setMinValue(isNaN(value) ? 0 : value)
+    debouncedUpdate(isNaN(value) ? 0 : value, maxValue)
   }
-  const changeValue2 = (e: ChangeEvent<HTMLInputElement>) => {
-    setValueNum2(parseInt(e.currentTarget.value))
+
+  const changeMaxValue = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = parseInt(e.currentTarget.value, 10)
+
+    setMaxValue(isNaN(value) ? 0 : value)
+    debouncedUpdate(minValue, isNaN(value) ? 0 : value)
   }
+
+  useEffect(() => {
+    debouncedUpdate(minValue, maxValue)
+  }, [minValue, maxValue, debouncedUpdate])
 
   return (
     <div className={s.container}>
       <div className={s.blockValue}>
-        <input onChange={changeValue1} type={'number'} value={valueNum1} />
+        <input onChange={changeMinValue} type={'number'} value={minValue} />
       </div>
       <Slider.Root
         className={s.slider}
         max={100}
         onValueChange={changeSliderValues}
         step={1}
-        value={[valueNum1, valueNum2]}
+        value={[minValue, maxValue]}
       >
         <Slider.Track className={s.sliderTrack}>
           <Slider.Range className={s.sliderRange} />
         </Slider.Track>
-        <Slider.Thumb aria-label={'Volume'} className={s.sliderThumb} />
-        <Slider.Thumb aria-label={'Volume'} className={s.sliderThumb} />
+        <Slider.Thumb aria-label={'Minimum Value'} className={s.sliderThumb} />
+        <Slider.Thumb aria-label={'Maximum Value'} className={s.sliderThumb} />
       </Slider.Root>
       <div className={s.blockValue}>
-        <input onChange={changeValue2} type={'number'} value={valueNum2} />
+        <input onChange={changeMaxValue} type={'number'} value={maxValue} />
       </div>
     </div>
   )
