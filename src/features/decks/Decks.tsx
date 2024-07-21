f54d7681503195388d5f8ee75f85d54c9908850c
k2 <izmail.k2@gmail.com>
2024-07-21 14:14:55 +0300

    feat: added sorting for all cards and author in TabSwitcher component

--- src/features/decks/Decks.tsx
+++ src/features/decks/Decks.tsx
@@ -12,16 +12,18 @@ import { Table } from '@/commn/components/ui/tables/Table'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 import { CreateDeck } from '@/features/decks/createDeck/CreateDeck'
 import { Page } from '@/features/pages/Page'
+import { useGetCurrentUserDataQuery } from '@/services/auth/authService'
 import { useGetDecksQuery } from '@/services/decks/decksService'
 
 import s from './Decks.module.scss'
 
 export const Decks = () => {
   //rrm search params ------------
-  const [searchParams, setSearchParams] = useSearchParams({ name: '', page: '1' })
+  const [searchParams, setSearchParams] = useSearchParams({ authorId: '', name: '', page: '' })
 
   const page = Number(searchParams.get('page'))
   const name = searchParams.get('name')
+  const authorId = searchParams.get('authorId')
   const setPage = (page: number) => {
     searchParams.set('page', page.toString())
     setSearchParams(searchParams)
@@ -38,14 +40,27 @@ export const Decks = () => {
     setSearchParams(searchParams)
   }
 
+  const setAuthorDecks = (authorId: string) => {
+    const newParams = new URLSearchParams(searchParams.toString())
+
+    if (authorId === '') {
+      newParams.delete('authorId')
+    } else {
+      newParams.set('authorId', authorId.toString())
+    }
+    setSearchParams(newParams)
+  }
   const [itemPage, setItemPage] = useState(10)
 
   const { data, error, isError, isLoading } = useGetDecksQuery({
+    authorId: authorId ?? undefined,
     // как useEffect поулчаем данные
     currentPage: page || 1, //было просто page если use useState
     itemsPerPage: itemPage,
     name: name ?? undefined,
   }) //из query возвращается обьект из mutation картэш(массив с заранее определенными элементами)
+
+  const { data: dataUserData } = useGetCurrentUserDataQuery()
 
   if (isLoading) {
     return <Loading />
@@ -74,8 +89,12 @@ export const Decks = () => {
         <div className={s.tab}>
           <TabSwitcher
             tabInfo={[
-              { description: '', trigger: 'MyCards', value: '1' },
-              { description: '', trigger: 'AllCards', value: '2' },
+              {
+                callback: setAuthorDecks,
+                trigger: 'My Cards',
+                value: dataUserData?.id ?? '',
+              },
+              { callback: setAuthorDecks, trigger: 'All Cards', value: 'default' },
             ]}
           />
         </div>
