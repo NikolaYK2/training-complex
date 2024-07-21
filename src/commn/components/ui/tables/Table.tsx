06931bae5d6660f105d330329520f2f583bb8e06
k2 <izmail.k2@gmail.com>
2024-07-21 12:17:38 +0300

    feat: add state preview

--- src/commn/components/ui/tables/Table.tsx
+++ src/commn/components/ui/tables/Table.tsx
@@ -1,5 +1,6 @@
-import { ReactElement, useRef } from 'react'
+import { ReactElement, useRef, useState } from 'react'
 
+import { FilePreviewPortal } from '@/commn/components/ui/filePreviewPortal/FilePreviewPortal'
 import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 
@@ -12,7 +13,7 @@ type HeadersType = {
 
 type CellType = {
   element?: ReactElement[]
-  img?: string
+  img?: null | string
   value: string
 }
 
@@ -27,6 +28,7 @@ type TableProps = {
 }
 
 export const Table = ({ headers, paragraphs }: TableProps) => {
+  const [activeImg, setActiveImg] = useState<null | string>(null)
   const textErrors = 'Количество заголовков и колонок в строках должно совпадать'
   const ref = useRef<HTMLDivElement | null>(null)
   const isActiveSort = false
@@ -41,51 +43,55 @@ export const Table = ({ headers, paragraphs }: TableProps) => {
   }
 
   return (
-    <table className={s.container}>
-      <thead>
-        <tr>
-          {headers.map(header => (
-            <th className={s.header} key={header.id}>
-              <div className={`${s.caption} ${s.isCaption}`}>
-                <TextFormat variant={'subtitle2'}>{header.title}</TextFormat>
-
-                {isActiveSort && (
-                  <div className={s.sort} ref={ref}>
-                    <IconSvg name={'arrow'} />
-                  </div>
-                )}
-              </div>
-            </th>
-          ))}
-        </tr>
-      </thead>
-      <tbody>
-        {paragraphs.map(paragraph => (
-          <tr key={paragraph.idCells}>
-            {paragraph.cells.map((cell, idx) => (
-              <td className={s.row} key={idx}>
-                <div className={s.item}>
-                  {cell.img && (
-                    <div className={s.img}>
-                      <img alt={''} src={cell.img} />
+    <>
+      <table className={s.container}>
+        <thead>
+          <tr>
+            {headers.map(header => (
+              <th className={s.header} key={header.id}>
+                <div className={`${s.caption} ${s.isCaption}`}>
+                  <TextFormat variant={'subtitle2'}>{header.title}</TextFormat>
+                  {isActiveSort && (
+                    <div className={s.sort} ref={ref}>
+                      <IconSvg name={'arrow'} />
                     </div>
                   )}
-
-                  <TextFormat variant={'body2'}>{cell.value}</TextFormat>
-
-                  <div className={s.elements}>
-                    {cell.element?.map((el, k) => (
-                      <div className={s.element} key={k}>
-                        {el}
-                      </div>
-                    ))}
-                  </div>
                 </div>
-              </td>
-            )) || []}
+              </th>
+            ))}
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {paragraphs.map(paragraph => (
+            <tr key={paragraph.idCells}>
+              {paragraph.cells.map((cell, idx) => (
+                <td className={s.row} key={idx}>
+                  <div className={s.item}>
+                    {cell.img && (
+                      <div className={s.img}>
+                        <img
+                          alt={'img'}
+                          onClick={() => setActiveImg(cell.img ? cell.img : '')}
+                          src={cell.img}
+                        />
+                      </div>
+                    )}
+                    <TextFormat variant={'body2'}>{cell.value}</TextFormat>
+                    <div className={s.elements}>
+                      {cell.element?.map((el, k) => (
+                        <div className={s.element} key={k}>
+                          {el}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </td>
+              )) || []}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {activeImg && <FilePreviewPortal onClose={() => setActiveImg(null)} src={activeImg} />}
+    </>
   )
 }
