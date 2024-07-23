e69105b5c350024896fa4de4e09d88b79a1186ce
k2 <izmail.k2@gmail.com>
2024-07-24 00:45:02 +0300

    feat: add BackTo component for navigating back in history

---
+++ src/commn/components/ui/backTo/BackTo.tsx
@@ -0,0 +1,27 @@
+import { useNavigate } from 'react-router-dom'
+
+import { Button } from '@/commn/components/ui/button'
+import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
+import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+
+import s from './BackTo.module.scss'
+
+type Props = {
+  nameLink: string
+}
+export const BackTo = ({ nameLink }: Props) => {
+  const navigate = useNavigate()
+  const handleGoBack = () => navigate(-1)
+
+  return (
+    <Button className={s.containerBackTo} onClick={handleGoBack} variant={'link'}>
+      <div className={s.icon}>
+        <IconSvg name={'backTo'} />
+        <IconSvg name={'backTo'} />
+      </div>
+      <TextFormat className={s.text} variant={'body2'}>
+        {nameLink}
+      </TextFormat>
+    </Button>
+  )
+}
