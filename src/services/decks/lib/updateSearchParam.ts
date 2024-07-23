01fe982f1e6c95c73c2c4de14b95bbc03d68a716
k2 <izmail.k2@gmail.com>
2024-07-24 00:34:25 +0300

    feat: add callback

--- src/services/decks/lib/updateSearchParam.ts
+++ src/services/decks/lib/updateSearchParam.ts
@@ -2,12 +2,16 @@ export const updateSearchParam = (
   searchParams: URLSearchParams,
   setSearchParams: (params: URLSearchParams) => void,
   key: string,
-  value: number | string
+  value: number | string,
+  callBack?: (value: number) => void
 ) => {
   if (value === '' || value === 0) {
     searchParams.delete(key)
   } else {
     searchParams.set(key, value.toString())
+  }
+  if (value && callBack) {
+    callBack(1)
   }
   setSearchParams(searchParams)
 }
