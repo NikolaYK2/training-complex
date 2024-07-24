d3fa831966dce08572935b1423896f9346bedb9b
k2 <izmail.k2@gmail.com>
2024-07-24 19:34:32 +0300

    refactor: add prop for save path

--- src/commn/components/ui/backTo/BackTo.tsx
+++ src/commn/components/ui/backTo/BackTo.tsx
@@ -3,15 +3,20 @@ import { useNavigate } from 'react-router-dom'
 import { Button } from '@/commn/components/ui/button'
 import { IconSvg } from '@/commn/components/ui/iconSvg/IconSvg'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+import { DECK_ROUTE } from '@/routes/Router'
 
 import s from './BackTo.module.scss'
 
 type Props = {
   nameLink: string
+  saveHistoryPage?: null | string
 }
-export const BackTo = ({ nameLink }: Props) => {
+export const BackTo = ({ nameLink, saveHistoryPage }: Props) => {
   const navigate = useNavigate()
-  const handleGoBack = () => navigate(-1)
+
+  const handleGoBack = () => {
+    navigate(`${DECK_ROUTE}?page=${saveHistoryPage}`)
+  }
 
   return (
     <Button className={s.containerBackTo} onClick={handleGoBack} variant={'link'}>
