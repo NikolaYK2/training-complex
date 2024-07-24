6e81181142b1ed74d38922a7ff1f6ccb9db65184
k2 <izmail.k2@gmail.com>
2024-07-24 20:21:38 +0300

    feat: add props - Component pre-expands the image on click if it exists

--- src/commn/components/ui/title/Title.tsx
+++ src/commn/components/ui/title/Title.tsx
@@ -1,19 +1,32 @@
-import { ReactNode } from 'react'
+import { CSSProperties, ReactNode, useState } from 'react'
 
+import { FilePreviewPortal } from '@/commn/components/ui/filePreviewPortal/FilePreviewPortal'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 
 import s from './Title.module.scss'
 
 type Props = {
   children: ReactNode
+  imageTitle?: null | string
+  marginBot?: CSSProperties['marginBottom']
   name: string
 }
+export const Title = ({ children, imageTitle, marginBot, name }: Props) => {
+  const styles: CSSProperties = { marginBottom: marginBot }
+  const [isActivePreview, setActivePreview] = useState(false)
 
-export const Title = ({ children, name }: Props) => {
   return (
-    <div className={s.containerTitle}>
-      <TextFormat variant={'h1'}>{name}</TextFormat>
-      {children}
+    <div className={s.containerTitle} style={styles}>
+      <div className={s.blockItem}>
+        <TextFormat variant={'h1'}>{name}</TextFormat>
+        {children}
+      </div>
+      {imageTitle && (
+        <div className={s.imageTitle} onClick={() => setActivePreview(true)}>
+          <img alt={'image'} src={imageTitle} />
+        </div>
+      )}
+      {isActivePreview && <FilePreviewPortal onClose={setActivePreview} src={imageTitle ?? ''} />}
     </div>
   )
 }
