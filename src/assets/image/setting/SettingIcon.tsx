61a6de902378563362b0d18fdaffc7833b5c76bb
k2 <izmail.k2@gmail.com>
2024-07-25 20:28:31 +0300

    feat: add icon SettingIcon & LearnIcon

---
+++ src/assets/image/setting/SettingIcon.tsx
@@ -0,0 +1,25 @@
+export const SettingIcon = () => {
+  return (
+    <svg
+      fill={'none'}
+      height={'18'}
+      viewBox={'0 0 18 18'}
+      width={'18'}
+      xmlns={'http://www.w3.org/2000/svg'}
+    >
+      <circle cx={'9'} cy={'9'} r={'8.5'} stroke={'#fff'}></circle>
+      <g clipPath={'url(#clip0_84039_3192)'} fill={'#fff'}>
+        <path
+          d={
+            'M9 10a1 1 0 100-2 1 1 0 000 2zM9 6.5a1 1 0 100-2 1 1 0 000 2zM9 13.5a1 1 0 100-2 1 1 0 000 2z'
+          }
+        ></path>
+      </g>
+      <defs>
+        <clipPath id={'clip0_84039_3192'}>
+          <path d={'M0 0H12V12H0z'} fill={'#fff'} transform={'translate(3 3)'}></path>
+        </clipPath>
+      </defs>
+    </svg>
+  )
+}
