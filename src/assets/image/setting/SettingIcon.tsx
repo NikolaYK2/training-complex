6738f759f8cd418fd5ea1d28d31caa9ff729a9c6
k2 <izmail.k2@gmail.com>
2024-07-25 21:29:07 +0300

    refactor: add style animation

--- src/assets/image/setting/SettingIcon.tsx
+++ src/assets/image/setting/SettingIcon.tsx
@@ -1,25 +1,30 @@
+import s from './SettingIcon.module.scss'
+
 export const SettingIcon = () => {
   return (
-    <svg
-      fill={'none'}
-      height={'18'}
-      viewBox={'0 0 18 18'}
-      width={'18'}
-      xmlns={'http://www.w3.org/2000/svg'}
-    >
-      <circle cx={'9'} cy={'9'} r={'8.5'} stroke={'#fff'}></circle>
-      <g clipPath={'url(#clip0_84039_3192)'} fill={'#fff'}>
-        <path
-          d={
-            'M9 10a1 1 0 100-2 1 1 0 000 2zM9 6.5a1 1 0 100-2 1 1 0 000 2zM9 13.5a1 1 0 100-2 1 1 0 000 2z'
-          }
-        ></path>
-      </g>
-      <defs>
-        <clipPath id={'clip0_84039_3192'}>
-          <path d={'M0 0H12V12H0z'} fill={'#fff'} transform={'translate(3 3)'}></path>
-        </clipPath>
-      </defs>
-    </svg>
+    <div className={s.containerSettingIcon}>
+      <svg
+        fill={'none'}
+        height={'100%'}
+        viewBox={'-1.5 -1.5 21 21'}
+        width={'100%'}
+        xmlns={'http://www.w3.org/2000/svg'}
+      >
+        <circle className={s.circle} cx={'8.8'} cy={'8.8'} r={'8.5'} stroke={'#fff'}></circle>
+        <g clipPath={'url(#clip0_84039_3192)'} fill={'#fff'}>
+          <path
+            d={
+              'M9 10a1 1 0 100-2 1 1 0 000 2zM9 6.5a1 1 0 100-2 1 1 0 000 2zM9 13.5a1 1 0 100-2 1 1 0 000 2z'
+            }
+            fill={'#fff'}
+          ></path>
+        </g>
+        <defs>
+          <clipPath id={'clip0_84039_3192'}>
+            <path d={'M0 0H12V12H0z'} fill={'#fff'} transform={'translate(3 3)'}></path>
+          </clipPath>
+        </defs>
+      </svg>
+    </div>
   )
 }
