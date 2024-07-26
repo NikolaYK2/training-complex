d6f21ad91987ce0e6a395703965aaaa9c5d8abca
k2 <izmail.k2@gmail.com>
2024-07-26 23:15:19 +0300

    refactor: Refactor Radio component to use forwardRef - Improved readability by destructuring props in the Radio - upd style

--- src/commn/components/ui/radioGroup/Radio.tsx
+++ src/commn/components/ui/radioGroup/Radio.tsx
@@ -1,24 +1,29 @@
-import { FC } from 'react'
+import { forwardRef } from 'react'
 
 import * as RadioGroup from '@radix-ui/react-radio-group'
 
 import s from './Radio.module.scss'
 
-type Props = {
+export type RadioProps = {
   disabled?: boolean
   id: string
+  label?: string
   value: string
 }
 
-export const Radio: FC<Props> = ({ disabled, id, value }) => {
+export const Radio = forwardRef<HTMLButtonElement, RadioProps>((props, ref) => {
+  const { disabled, id, label, value, ...rest } = props
+
   return (
     <div className={`${s.blockRadio}`}>
       <label className={`${s.blockItem} ${disabled ? s.disabledLabel : ''}`}>
-        <RadioGroup.Item className={s.item} id={id} value={value}>
+        <RadioGroup.Item className={s.item} id={value} ref={ref} value={value} {...rest}>
           <RadioGroup.Indicator className={s.indicator} />
         </RadioGroup.Item>
       </label>
-      <label className={`${s.label} ${disabled ? s.disabledLabel : ''}`}>{value}</label>
+      <label className={`${s.label} ${disabled ? s.disabledLabel : ''}`} htmlFor={id}>
+        {label}
+      </label>
     </div>
   )
-}
+})
