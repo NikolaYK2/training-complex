46e1b7d0b5e85f94bf6ef179ef580445fb24cc30
k2 <izmail.k2@gmail.com>
2024-07-26 23:28:23 +0300

    refactor: add prop for displaying title card

--- src/commn/components/ui/card/Card.tsx
+++ src/commn/components/ui/card/Card.tsx
@@ -1,13 +1,20 @@
 import { ComponentProps } from 'react'
 
+import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+
 import s from './Card.module.scss'
 
-type Props = ComponentProps<'div'>
+type Props = ComponentProps<'div'> & {
+  title?: string
+}
 
-export const Card = ({ className, ...rest }: Props) => {
+export const Card = ({ children, className, title, ...rest }: Props) => {
   return (
     <div className={s.container}>
-      <div className={`${s.blockCard} ${className}`} {...rest} />
+      <div className={`${s.blockCard} ${className}`} {...rest}>
+        {title && <TextFormat variant={'h1'}>{`Learn ${title}`}</TextFormat>}
+        {children}
+      </div>
     </div>
   )
 }
