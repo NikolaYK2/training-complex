060e797405625021d9386d148375d4ff992633ca
k2 <izmail.k2@gmail.com>
2024-07-29 22:14:50 +0300

    feat: add DeleteCard.tsx

---
+++ src/features/cards/deleteCard/DeleteCard.tsx
@@ -0,0 +1,65 @@
+import { SubmitHandler } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
+
+import { Loading } from '@/commn/components/ui/loading/Loading'
+import { DialogModal } from '@/commn/components/ui/modals/dialog/DialogModal'
+import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
+import { useCreateEntityForm } from '@/commn/hooks/useCreateEntityForm'
+import { DECK_ROUTE } from '@/routes/Router'
+import { useDeleteDeckMutation } from '@/services/decks/decksService'
+import { z } from 'zod'
+
+const deleteCardSchema = z.object({
+  id: z.string(),
+})
+
+type FormType = z.infer<typeof deleteCardSchema>
+type Props = {
+  idCard: string | undefined
+  nameDeck: string | undefined
+}
+export const DeleteCard = ({ idCard, nameDeck }: Props) => {
+  const navigate = useNavigate()
+
+  const { handleSubmit } = useCreateEntityForm({
+    defaultValues: {
+      id: idCard ?? '',
+    },
+    schema: deleteCardSchema,
+  })
+  const [
+    deleteDeck,
+    { error: errorDeleteDeck, isError: isErrorDeleteDeck, isLoading: isLoadingDeleteDeck },
+  ] = useDeleteDeckMutation()
+
+  const onSubmit: SubmitHandler<FormType> = async data => {
+    if (data.id) {
+      await deleteDeck({ id: data.id })
+      navigate(`${DECK_ROUTE}`)
+    }
+  }
+
+  if (isErrorDeleteDeck) {
+    return <div>Error: {JSON.stringify(errorDeleteDeck)}</div>
+  }
+  if (isLoadingDeleteDeck) {
+    return <Loading />
+  }
+
+  return (
+    <DialogModal
+      onSubmit={handleSubmit(onSubmit)}
+      titleContent={'delete pack'}
+      triggerVariant={'link'}
+    >
+      {[
+        <TextFormat key={'text-1'} variant={'subtitle1'}>
+          {`Do you really want to remove ${nameDeck ? nameDeck : 'name deck'} `}
+        </TextFormat>,
+        <TextFormat key={'text-2'} variant={'subtitle1'}>
+          All cards will be deleted.
+        </TextFormat>,
+      ]}
+    </DialogModal>
+  )
+}
