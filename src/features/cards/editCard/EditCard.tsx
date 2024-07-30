f4cecbed91b9dba045700091a6596aab0d8bd815
k2 <izmail.k2@gmail.com>
2024-07-31 01:21:36 +0300

    feat: add props - map parameter transfer

--- src/features/cards/editCard/EditCard.tsx
+++ src/features/cards/editCard/EditCard.tsx
@@ -1,31 +1,50 @@
-import { CreateUpdateDeck } from '@/commn/components/ui/createUpdateDeck/CreateUpdateDeck'
-import { useCreateUpdateDeckMutation } from '@/services/decks/decksService'
+import { EditIcon } from '@/assets/image/edit/EditIcon'
+import { CreateUpdateCard } from '@/commn/components/ui/createUpdateCard/CreateUpdateCard'
+import { useUpdateCardMutation } from '@/services/cards/cardsService'
+import { CardsResponse } from '@/services/decks/DecksTypes'
+
+import s from './EditCard.module.scss'
 
 type Props = {
-  cover: null | string | undefined
-  idCard: string | undefined
-  isPrivateCard: boolean | undefined
-  name: string | undefined
+  answer: string
+  answerImg: string
+  className?: string
+  dataGetCardById?: CardsResponse | undefined
+  idCard?: string
+  question: string
+  questionImg: string
 }
-export const EditCard = ({ cover, idCard, isPrivateCard, name }: Props) => {
+export const EditCard = ({
+  answer,
+  answerImg,
+  className = '',
+  idCard,
+  question,
+  questionImg,
+}: Props) => {
   const [
-    updateDeck,
-    { error: errUpdateDeck, isError: isErrUpdateDeck, isLoading: isLoadUpdateDeck },
-  ] = useCreateUpdateDeckMutation()
+    updateCard,
+    { error: errorUpdateCard, isError: isErrorUpdateCard, isLoading: isLoadingUpdateCard },
+  ] = useUpdateCardMutation()
 
   return (
-    <CreateUpdateDeck
-      coverDeckBy={cover}
-      error={errUpdateDeck}
-      idCard={idCard}
-      isError={isErrUpdateDeck}
-      isLoading={isLoadUpdateDeck}
-      isPrivateCard={isPrivateCard}
-      method={'PATCH'}
-      mutationFunction={updateDeck}
-      nameDeckBy={name}
-      titleContent={'edit pack'}
-      triggerVariant={'link'}
-    />
+    <label className={`${s.containerEditCard} ${className}`}>
+      <EditIcon className={s.icon} />
+      <CreateUpdateCard
+        answer={answer}
+        answerImg={answerImg}
+        buttonName={'save change'}
+        cardId={idCard}
+        className={s.updateCard}
+        error={errorUpdateCard}
+        isError={isErrorUpdateCard}
+        isLoading={isLoadingUpdateCard}
+        mutationFunction={updateCard}
+        question={question}
+        questionImg={questionImg}
+        titleContent={'edit Card'}
+        triggerVariant={'link'}
+      />
+    </label>
   )
 }
