ac126b23f21519b7bfca129b8cae4d2d2347ebed
k2 <izmail.k2@gmail.com>
2024-07-31 01:22:21 +0300

    feat: add EditCards.tsx - change card

---
+++ src/features/cards/editCards/EditCards.tsx
@@ -0,0 +1,32 @@
+import { CreateUpdateDeck } from '@/commn/components/ui/createUpdateDeck/CreateUpdateDeck'
+import { useCreateUpdateDeckMutation } from '@/services/decks/decksService'
+
+type Props = {
+  cover: null | string | undefined
+  idCard: string | undefined
+  isPrivateCard: boolean | undefined
+  name: string | undefined
+}
+export const EditCards = ({ cover, idCard, isPrivateCard, name }: Props) => {
+  const [
+    updateDeck,
+    { error: errUpdateDeck, isError: isErrUpdateDeck, isLoading: isLoadUpdateDeck },
+  ] = useCreateUpdateDeckMutation()
+
+  return (
+    <CreateUpdateDeck
+      buttonName={'change save'}
+      coverDeckBy={cover}
+      error={errUpdateDeck}
+      idCard={idCard}
+      isError={isErrUpdateDeck}
+      isLoading={isLoadUpdateDeck}
+      isPrivateCard={isPrivateCard}
+      method={'PATCH'}
+      mutationFunction={updateDeck}
+      nameDeckBy={name}
+      titleContent={'edit pack'}
+      triggerVariant={'link'}
+    />
+  )
+}
