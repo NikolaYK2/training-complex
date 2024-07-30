e9b358cec634b7b5f4549eee47d3b71e09d538cf
k2 <izmail.k2@gmail.com>
2024-07-31 01:01:33 +0300

    refactor: move formData logic to prepareFormData - add types

--- src/services/cards/CardsServiceType.ts
+++ src/services/cards/CardsServiceType.ts
@@ -1,1 +1,16 @@
-export type {}
+export type CardArgs = {
+  answer: string
+  answerImg?: string
+  answerVideo?: string
+  id: string
+  question: string
+  questionImg?: string
+  questionVideo?: string
+}
+export type CardResponse = CardArgs & {
+  created: string
+  deckId: string
+  shots: number
+  updated: string
+  userId: string
+}
--- src/services/cards/cardsService.ts
+++ src/services/cards/cardsService.ts
@@ -1,13 +1,32 @@
+import { CardArgs, CardResponse } from '@/services/cards/CardsServiceType'
+import { CardsResponse } from '@/services/decks/DecksTypes'
 import { flashcardsApi } from '@/services/flashcardsApi'
+import { prepareFormData } from '@/services/lib/prepareFormData'
+
+const CARDS = 'v1/cards/'
 
 export const cardsService = flashcardsApi.injectEndpoints({
   endpoints: builder => ({
-    getCards: builder.query<void, { id: string }>({
+    getCardById: builder.query<CardsResponse, { id: string }>({
+      providesTags: ['Cards'],
       query: ({ id }) => {
         return {
-          url: `v1/cards/${id}`,
+          url: `${CARDS}${id}`,
+        }
+      },
+    }),
+    updateCard: builder.mutation<CardResponse, CardArgs>({
+      invalidatesTags: ['Cards'],
+      query: ({ answer, answerImg, id, question, questionImg }) => {
+        const formData = prepareFormData({ answer, answerImg, question, questionImg })
+
+        return {
+          body: formData,
+          method: 'PATCH',
+          url: `${CARDS}${id}`,
         }
       },
     }),
   }),
 })
+export const { useUpdateCardMutation } = cardsService
