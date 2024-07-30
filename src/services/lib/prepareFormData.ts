6305dd2b0c3a881e45279f2327b382dcdbe9b121
k2 <izmail.k2@gmail.com>
2024-07-31 01:53:06 +0300

    feat: add prepareFormData.ts

---
+++ src/services/lib/prepareFormData.ts
@@ -0,0 +1,54 @@
+type PrepareFormDataProps = {
+  answer?: string
+  answerImg?: File | string
+  cover?: File | string
+  initialAnswerImg?: string
+  initialQuestionImg?: string
+  isPrivate?: boolean
+  name?: string
+  question?: string
+  questionImg?: File | string
+}
+export const prepareFormData = ({
+  answer,
+  answerImg,
+  cover,
+  isPrivate,
+  name,
+  question,
+  questionImg,
+}: PrepareFormDataProps): FormData => {
+  const formData = new FormData()
+
+  appendFieldImage('questionImg', questionImg, formData)
+  appendFieldImage('answerImg', answerImg, formData)
+  appendFieldImage('cover', cover, formData)
+
+  appendFieldText('name', name, formData)
+  appendFieldText('isPrivate', isPrivate, formData)
+  appendFieldText('answer', answer, formData)
+  appendFieldText('question', question, formData)
+
+  return formData
+}
+
+const appendFieldImage = (
+  key: string,
+  file: File | string | undefined,
+  formData: FormData
+): void => {
+  if (file instanceof File) {
+    formData.append(key, file)
+  } else if (file === null) {
+    formData.append(key, '')
+  }
+}
+const appendFieldText = (
+  key: string,
+  value: boolean | string | undefined,
+  formData: FormData
+): void => {
+  if (value !== undefined && value !== '') {
+    formData.append(key, value.toString())
+  }
+}
