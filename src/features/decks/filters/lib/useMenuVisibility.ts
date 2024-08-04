5b2cb530c88e476e95ab7c1e0e84bbee34558052
k2 <izmail.k2@gmail.com>
2024-08-04 19:16:32 +0300

    feat: add handler

--- src/features/decks/filters/lib/useMenuVisibility.ts
+++ src/features/decks/filters/lib/useMenuVisibility.ts
@@ -1,14 +1,17 @@
-import { useEffect, useState } from 'react'
+import { MouseEvent, useEffect, useState } from 'react'
 
 export const useMenuVisibility = () => {
   const [isMenuVisible, setMenuVisible] = useState(false)
   const handleToggleMenu = () => {
     setMenuVisible(prev => !prev)
   }
+  const handleClickStopPropagation = (e: MouseEvent<HTMLDivElement>) => {
+    e.stopPropagation()
+  }
 
   useEffect(() => {
     document.body.style.overflow = isMenuVisible ? 'hidden' : 'unset'
   }, [isMenuVisible])
 
-  return { handleToggleMenu, isMenuVisible }
+  return { handleClickStopPropagation, handleToggleMenu, isMenuVisible }
 }
