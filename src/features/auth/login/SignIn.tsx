c525d74d4ff080ac62ac3f2acc05fbcb5d6e3b4f
k2 <izmail.k2@gmail.com>
2024-08-06 00:38:39 +0300

    feat: add redirect on HOME if authorisation passed

--- src/features/auth/login/SignIn.tsx
+++ src/features/auth/login/SignIn.tsx
@@ -1,23 +1,25 @@
 import { SubmitHandler, useForm } from 'react-hook-form'
+import { useNavigate } from 'react-router-dom'
 
 import { Card } from '@/commn/components/ui/card/Card'
 import { FormAuth } from '@/commn/components/ui/formAuth/FormAuth'
 import { Loading } from '@/commn/components/ui/loading/Loading'
 import { Page } from '@/commn/components/ui/pages/Page'
-import { localStorageUtil } from '@/commn/utils/localStorageUtil'
+import { HOME_ROUTE } from '@/routes/Router'
 import { useLoginAuthMutation } from '@/services/auth/authService'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().trim().email(),
-  password: z.string().min(5),
+  password: z.string().min(3).max(30),
   remember: z.boolean().default(false), // Добавить default значение,
 })
 
 export type LoginFormType = z.infer<typeof loginSchema>
 
 export const SignIn = () => {
+  const navigate = useNavigate()
   const {
     control,
     formState: { errors },
@@ -36,24 +38,30 @@ export const SignIn = () => {
 
   const onSubmit: SubmitHandler<LoginFormType> = async data => {
     try {
-      const tokensRes = await authorisation({
+      await authorisation({
         email: data.email,
         password: data.password,
         rememberMe: data.remember,
       }).unwrap()
+      // const tokensRes = await authorisation({
+      //   email: data.email,
+      //   password: data.password,
+      //   rememberMe: data.remember,
+      // }).unwrap()
 
-      localStorageUtil.saveItem('accessToken', tokensRes?.accessToken)
-      localStorageUtil.saveItem('refreshToken', tokensRes?.refreshToken)
+      // localStorageUtil.saveItem('accessToken', tokensRes?.accessToken)
+      // localStorageUtil.saveItem('refreshToken', tokensRes?.refreshToken)
 
       reset()
+      navigate(HOME_ROUTE)
     } catch (e) {
       console.error('login error: ', e)
     }
   }
 
-  if (isError) {
-    return <div>{JSON.stringify(error)}</div>
-  }
+  // if (isError) {
+  //   return <div>{JSON.stringify(error)}</div>
+  // }
 
   return (
     <Page marginTop={'var(--margin-top-page)'}>
