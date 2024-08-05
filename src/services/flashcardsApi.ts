6e82ce600ba1387f6436b1e9cda0d6a69e38d546
k2 <izmail.k2@gmail.com>
2024-08-06 00:32:41 +0300

    refactor: remove prepareHeaders

--- src/services/flashcardsApi.ts
+++ src/services/flashcardsApi.ts
@@ -4,11 +4,6 @@ export const flashcardsApi = createApi({
   baseQuery: fetchBaseQuery({
     baseUrl: 'https://api.flashcards.andrii.es',
     credentials: 'include',
-    prepareHeaders: headers => {
-      headers.append('x-auth-skip', 'true')
-
-      return headers
-    },
   }),
   //можно эндпоинты разделять как бы на сущности
   endpoints: () => ({}), //нужно указат ьпустой эндпооинт обязательно
