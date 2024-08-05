1d268fc64dfb613ea71dccdef01b7400ebb2b69f
k2 <izmail.k2@gmail.com>
2024-08-06 00:35:00 +0300

    feat: added validation check in PrivateRoutes

--- src/routes/PrivateRoutes.tsx
+++ src/routes/PrivateRoutes.tsx
@@ -1,7 +1,19 @@
 import { Navigate, Outlet } from 'react-router-dom'
 
+import { Loading } from '@/commn/components/ui/loading/Loading'
+import { useGetCurrentUserDataQuery } from '@/services/auth/authService'
+
 export const PrivateRoutes = () => {
-  const isAuthenticated = true
+  const { error: errMe, isError: isErrMe, isLoading: isLoadMe } = useGetCurrentUserDataQuery()
+
+  if (isLoadMe) {
+    return <Loading />
+  }
+  // if (isErrMe) {
+  //   return <div>Error: {JSON.stringify(errMe)}</div>
+  // }
+
+  const isAuthenticated = !isErrMe //значит не авторизован
 
   return isAuthenticated ? <Outlet /> : <Navigate to={'/login'} />
 }
