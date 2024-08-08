44425a740f76f33156e15dbdb1a14cf4fc30b18f
k2 <izmail.k2@gmail.com>
2024-08-08 18:27:20 +0300

    feat: add baseQueryWithReauth
    - Implemented mechanism to automatically update token when 401 Unauthorised response is received.
    - Added mutex to prevent simultaneous execution of multiple token update requests.
    - In case of an unsuccessful token update, the user is redirected to the login page.

---
+++ src/services/flashcardsApiWithReauth.ts
@@ -0,0 +1,77 @@
+import { localStorageUtil } from '@/commn/utils/localStorageUtil'
+import { LOGIN_ROUTE, router } from '@/routes/Router'
+import { LoginResponse } from '@/services/auth/AuthTypes'
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  fetchBaseQuery,
+} from '@reduxjs/toolkit/query/react'
+import { Mutex } from 'async-mutex'
+
+type RefreshResultType = { data?: LoginResponse; error?: FetchBaseQueryError }
+const mutex = new Mutex()
+const baseQuery = fetchBaseQuery({
+  baseUrl: 'https://api.flashcards.andrii.es',
+  prepareHeaders: headers => {
+    const token = localStorageUtil.getItem('accessToken')
+
+    if (headers.get('Authorization')) {
+      return headers
+    }
+
+    if (token) {
+      headers.set('Authorization', `Bearer ${token}`)
+    }
+
+    return headers
+  },
+})
+
+export const baseQueryWithReauth: BaseQueryFn<
+  FetchArgs | string,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  await mutex.waitForUnlock()
+
+  let result = await baseQuery(args, api, extraOptions)
+
+  if (result.error && result.error.status === 401) {
+    if (!mutex.isLocked()) {
+      const release = await mutex.acquire()
+      const refreshToken = localStorageUtil.getItem('refreshToken')
+
+      try {
+        // Попытка получить новый токен
+        const refreshResult = (await baseQuery(
+          {
+            headers: {
+              Authorization: `Bearer ${refreshToken}`,
+            },
+            method: 'POST',
+            url: '/v2/auth/refresh-token',
+          },
+          api,
+          extraOptions
+        )) as RefreshResultType
+
+        if (refreshResult.data) {
+          localStorageUtil.saveItem('accessToken', refreshResult.data.accessToken)
+          localStorageUtil.saveItem('refreshToken', refreshResult.data.refreshToken)
+
+          result = await baseQuery(args, api, extraOptions)
+        } else {
+          router.navigate(LOGIN_ROUTE)
+        }
+      } finally {
+        release()
+      }
+    } else {
+      await mutex.waitForUnlock()
+      result = await baseQuery(args, api, extraOptions)
+    }
+  }
+
+  return result
+}
