a2e0f3f251e5fdabb0697fb400f97cdad6831c5e
k2 <izmail.k2@gmail.com>
2024-08-08 18:30:34 +0300

    refactor: simplify user data check and handle logout
    - Replaced manual check of `data` with `Boolean` conversion for clarity.
    - Added a simplified `handleLogOut` function to trigger logout.

--- src/features/1-header/profileMenu/ProfileMenu.tsx
+++ src/features/1-header/profileMenu/ProfileMenu.tsx
@@ -1,8 +1,7 @@
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 
 import { Button } from '@/commn/components/ui/button'
 import { DropDownMenu } from '@/commn/components/ui/dropDownMenu/DropDownMenu'
-import { Loading } from '@/commn/components/ui/loading/Loading'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 import { LOGIN_ROUTE, PROFILE_ROUTE } from '@/routes/Router'
 import { useGetCurrentUserDataQuery, useLogoutMutation } from '@/services/auth/authService'
@@ -10,31 +9,16 @@ import { useGetCurrentUserDataQuery, useLogoutMutation } from '@/services/auth/a
 import s from './ProfileMenu.module.scss'
 
 export const ProfileMenu = () => {
-  const navigate = useNavigate()
-  const { data, error, isError } = useGetCurrentUserDataQuery()
-  const [logout, { error: errorLogout, isError: isErrorLogout, isLoading: isLoadingLogout }] =
-    useLogoutMutation()
-  const isLogin = !isError
-
-  // if (isError) {
-  //   return <div>Error: {JSON.stringify(error)}</div>
-  // }
-  const handleLogOut = async () => {
-    await logout()
-    navigate(LOGIN_ROUTE)
-  }
-
-  if (isErrorLogout) {
-    return <div>Error: {JSON.stringify(errorLogout)}</div>
-  }
-
-  if (isLoadingLogout) {
-    return <Loading />
+  const { data, isError: isErrMe, isLoading: isLoadMe } = useGetCurrentUserDataQuery()
+  const me = Boolean(data)
+  const [logout] = useLogoutMutation()
+  const handleLogOut = () => {
+    logout()
   }
 
   return (
     <>
-      {isLogin ? (
+      {me ? (
         <DropDownMenu
           classNameMenuArrow={s.arrow}
           menuConfig={{
@@ -55,6 +39,7 @@ export const ProfileMenu = () => {
                 buttonName: 'sign out',
                 callback: handleLogOut,
                 classNameButton: 'iconLogOut',
+                disabled: isLoadMe,
                 icon: 'logOut',
               },
             ],
