7d74c9ff874b845efa5e0d761ed4eaadd31afdcb
k2 <izmail.k2@gmail.com>
2024-08-09 22:52:48 +0300

    feat: added exceptions for authorId and pageItem parameters in clearParams - Added a `paramsException` variable containing parameters that should not be deleted when clearing. - Modified `clearParams` so that excluded parameters are not removed. - add removeKeys for updateSearchParam

--- src/commn/hooks/useSearchUpdateParams.ts
+++ src/commn/hooks/useSearchUpdateParams.ts
@@ -1,8 +1,19 @@
 import { URLSearchParamsInit, useSearchParams } from 'react-router-dom'
 
+export const DECKS_KEY_SEARCH_PARAMS = {
+  authorId: 'authorId',
+  default: 'default',
+  favoritedBy: 'favoritedBy',
+  maxCardsCount: 'maxCardsCount',
+  minCardsCount: 'minCardsCount',
+  orderBy: 'orderBy',
+  page: 'page',
+  pageItem: 'itemsPerPage',
+  searchName: 'name',
+}
 export type UpdateSearchParamType = {
-  callBack?: (value: number) => void
   key: string
+  removeKeys?: string[]
   replace?: boolean
   value: null | number | string
 }
@@ -10,22 +21,35 @@ type SearchParamsInit = URLSearchParamsInit | undefined
 export const useSearchUpdateParams = <T extends SearchParamsInit>(props?: T) => {
   const [searchParams, setSearchParams] = useSearchParams(props)
 
-  const updateSearchParam = ({ callBack, key, replace = false, value }: UpdateSearchParamType) => {
+  const updateSearchParam = ({
+    key,
+    removeKeys,
+    replace = false,
+    value,
+  }: UpdateSearchParamType) => {
     if (value === '' || value === 0 || value === null) {
       searchParams.delete(key)
     } else {
       searchParams.set(key, value.toString())
     }
-    if (value && callBack) {
-      callBack?.(1)
+    if (removeKeys && Array.isArray(removeKeys)) {
+      removeKeys.forEach(key => searchParams.delete(key))
     }
     setSearchParams(searchParams, { replace })
   }
   const getSearchParam = (key: string, defaultValue: any = '') => {
     return searchParams.get(key) || defaultValue
   }
+  const paramsException = [DECKS_KEY_SEARCH_PARAMS.authorId, DECKS_KEY_SEARCH_PARAMS.pageItem]
+
+  const clearParams = () => {
+    Object.values(DECKS_KEY_SEARCH_PARAMS).forEach(
+      param => !paramsException.includes(param) && searchParams.delete(param)
+    )
+  }
 
   return {
+    clearParams,
     getSearchParam,
     searchParams,
     setSearchParams,
