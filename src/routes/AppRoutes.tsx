59b4c207a4aef6dfd160ec33abf9638ab44cb4ca
k2 <izmail.k2@gmail.com>
2024-08-10 22:13:46 +0300

    feat: add AuthContext type and useAuthContext hook
    - Implemented `useAuthContext` hook to access context values using `useOutletContext`.
    - Updated `AppRoutes` component to use `AuthContext` type with `Outlet` context.
      - Included loading state with `Loading` component.
      - Passed `isAuthenticated` context value to `Outlet` using `satisfies` operator to ensure type safety.

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,11 +1,28 @@
-import { Outlet } from 'react-router-dom'
+import { Outlet, useOutletContext } from 'react-router-dom'
 
 import { Layout } from '@/commn/components/ui/layout/Layout'
+import { Loading } from '@/commn/components/ui/loading/Loading'
+import { useGetCurrentUserDataQuery } from '@/services/auth/authService'
+
+type AuthContext = {
+  isAuthenticated: boolean
+}
+
+export function useAuthContext() {
+  return useOutletContext<AuthContext>()
+}
 
 export const AppRoutes = () => {
+  const { data: currentMe, isLoading: isLoadMe } = useGetCurrentUserDataQuery()
+  const isAuthenticated = Boolean(currentMe)
+
+  if (isLoadMe) {
+    return <Loading />
+  }
+
   return (
-    <Layout>
-      <Outlet />
+    <Layout avatar={currentMe?.avatar} email={currentMe?.email} name={currentMe?.name}>
+      <Outlet context={{ isAuthenticated } satisfies AuthContext} />
     </Layout>
   )
 }
