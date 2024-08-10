d6b324f7668d8879d5660cc3bafef45bf5c306ce
k2 <izmail.k2@gmail.com>
2024-08-10 22:11:52 +0300

    temp: add Toast

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Provider } from 'react-redux'
 
+import { Toast } from '@/commn/components/ui/toast/Toast'
 import { Router } from '@/routes/Router'
 import { store } from '@/services/store'
 
 export function App() {
   return (
     <Provider store={store}>
+      <Toast />
       <Router />
     </Provider>
   )
