678e5f3e5c4d764a5ad2320ec1e44941e70736fc
k2 <izmail.k2@gmail.com>
2024-08-13 14:20:25 +0300

    refactor: improved state of avatar display if there is no avatar delete account is now a component, the logic has been migrated

--- src/features/1-header/profileMenu/ProfileMenu.tsx
+++ src/features/1-header/profileMenu/ProfileMenu.tsx
@@ -6,27 +6,20 @@ import { DropDownMenu } from '@/commn/components/ui/dropDownMenu/DropDownMenu'
 import { LayoutProps } from '@/commn/components/ui/layout/Layout'
 import { TextFormat } from '@/commn/components/ui/typography/TextFormat'
 import { manageFeedback } from '@/commn/utils/manageFeedback'
+import { DeleteAccount } from '@/features/auth/deleteAccount/DeleteAccount'
 import { LOGIN_ROUTE, PROFILE_ROUTE } from '@/routes/Router'
-import { useDeleteAccountMutation, useLogoutMutation } from '@/services/auth/authService'
+import { useLogoutMutation } from '@/services/auth/authService'
 
 import s from './ProfileMenu.module.scss'
 
 export const ProfileMenu = ({ avatar, email, name }: LayoutProps) => {
   const me = Boolean(email)
   const [logout, { isLoading: isLoadLogOut }] = useLogoutMutation()
-  const [deleteAccount, { isLoading: isLoadingDeleteAcc }] = useDeleteAccountMutation()
   const dispatch = useDispatch()
 
   const handleLogOut = async () => {
     try {
       await logout()
-    } catch (e) {
-      manageFeedback({ data: e, dispatch, type: 'error' })
-    }
-  }
-  const handleDeleteAccount = async () => {
-    try {
-      await deleteAccount()
     } catch (e) {
       manageFeedback({ data: e, dispatch, type: 'error' })
     }
@@ -60,9 +53,12 @@ export const ProfileMenu = ({ avatar, email, name }: LayoutProps) => {
               },
               {
                 buttonName: 'delete account',
-                callback: handleDeleteAccount,
                 classNameButton: 'deleteAnimation',
-                disabled: isLoadingDeleteAcc,
+                element: (
+                  <DeleteAccount
+                    text={`Deleting the current user account /${email}? All the user data will be deleted forever. This action can not be undone`}
+                  />
+                ),
                 icon: 'delete',
               },
             ],
