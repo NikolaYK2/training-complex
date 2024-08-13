49d22aef4ba57455c43fbebb0d805c7f966a028b
k2 <izmail.k2@gmail.com>
2024-08-13 20:20:44 +0300

    feat: upd service updateCard - add onQueryStarted

--- src/services/cards/cardsService.ts
+++ src/services/cards/cardsService.ts
@@ -1,5 +1,6 @@
 import { CardArgs, CardResponse } from '@/services/cards/CardsServiceType'
 import { CardsResponse } from '@/services/decks/DecksTypes'
+import { decksService } from '@/services/decks/decksService'
 import { flashcardsApi } from '@/services/flashcardsApi'
 import { prepareFormData } from '@/services/lib/prepareFormData'
 
@@ -24,8 +25,33 @@ export const cardsService = flashcardsApi.injectEndpoints({
         }
       },
     }),
-    updateCard: builder.mutation<CardResponse, CardArgs>({
+    updateCard: builder.mutation<CardResponse[], CardArgs>({
       invalidatesTags: ['Cards'],
+      async onQueryStarted({ id, ...args }, { dispatch, getState, queryFulfilled }) {
+        const invalidateBy = cardsService.util.selectInvalidatedBy(getState(), ['Cards'])
+
+        const pathResult: any[] = []
+
+        invalidateBy.forEach(({ originalArgs }) => {
+          pathResult.push(
+            dispatch(
+              decksService.util.updateQueryData('retrieveCardsInDeck', originalArgs, draft => {
+                const itemToUpdIndex = draft.items.findIndex(card => card.id === id)
+
+                if (itemToUpdIndex === -1) {
+                  return
+                }
+                Object.assign(draft.items[itemToUpdIndex], args)
+              })
+            )
+          )
+        })
+        try {
+          await queryFulfilled
+        } catch (e) {
+          pathResult.forEach(patchResult => patchResult.undo())
+        }
+      },
       query: ({ answer, answerImg, id, question, questionImg }) => {
         const formData = prepareFormData({ answer, answerImg, question, questionImg })
 
